0d9132720ba9bd203150fb0633b564f68031572b
Matt Davis <matt.davis129@gmail.com>
2014-09-19 15:02:11 -0700

    builds out the my_each method

--- warmup/warmup.rb
+++ warmup/warmup.rb
@@ -1,1 +1,21 @@
-# Your code here!
+class Array
+
+	def my_each
+		i = 0
+		while i < self.length
+			yield(self[i])
+			i += 1
+		end
+		self
+	end
+
+
+
+	def my_stuff
+		self.reverse
+	end
+
+
+end
+
+
