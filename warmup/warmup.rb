d8f2dd4dac9c46c146d2003155f1efa2c9a4a844
Michael Alexander <alexandermw@gmail.com>
2014-09-19 20:23:03 -0700

    finish warmup in 1 line apiece

--- warmup/warmup.rb
+++ warmup/warmup.rb
@@ -1,10 +1,10 @@
 class Array
 
-  def my_each
+  def my_each(proc=nil)
     index = 0
 
-    while index <= self.length-1
-      yield(self[index])
+    while index <= self.length - 1
+      block_given? ? yield(self[index]) : proc.call(self[index])
       index += 1
     end
 
@@ -14,27 +14,16 @@ class Array
   def my_map(proc=nil)
     duplicate = []
 
-    self.my_each do |x|
-      if block_given?
-        duplicate << yield(x)
-      elsif proc
-
-      else
-
-      end
-    end
+    self.my_each {|x| proc ? duplicate << proc.call(x) : duplicate << yield(x) }
 
     duplicate
   end
 
-  def my_select
+  def my_select(proc=nil)
     selected_array = []
 
-    self.my_each do |x|
-      if yield(x)
-        selected_array << x
-      end
-    end
+    #HORRIBLE/AWESOME
+    self.my_each { |x| proc ? (selected_array << x if proc.call(x)) : (selected_array << x if yield(x)) }
 
     selected_array
   end
@@ -43,6 +32,33 @@ end
 
 array = ['Elephant', 'Lion', 'Gazelle']
 
+puts_proc = Proc.new { |x| puts x }
+upcase_proc = Proc.new { |x| x.upcase }
+elephant_proc = Proc.new { |x| x == 'Elephant' }
+
 array.my_each { |x| puts x }
 puts array.my_map { |x| x.upcase }
 puts array.my_select { |x| x == 'Elephant' }
+puts "-----------"
+array.my_each(&puts_proc)
+puts array.my_map(&upcase_proc)
+puts array.my_select(&elephant_proc)
+
+
+# OUTPUT
+#
+# Elephant
+# Lion
+# Gazelle
+# ELEPHANT
+# LION
+# GAZELLE
+# Elephant
+# -----------
+# Elephant
+# Lion
+# Gazelle
+# ELEPHANT
+# LION
+# GAZELLE
+# Elephant
