69320c5f57e9f4d3cf219237bdfcbf194240d18a
Tom McLaughlin <tim5046@gmail.com>
2014-09-19 23:06:23 -0400

    finished RPS

--- rock_paper_scissors/rock_paper_scissors.rb
+++ rock_paper_scissors/rock_paper_scissors.rb
@@ -2,39 +2,101 @@
 
 class Game
   def initialize
+    @player=Player.new
+  end
+
+  def unicode(arg)
+    case arg
+    when 'R'
+      "\u{270A}"
+    when 'S'
+      "\u{2704}"
+    when 'P'
+      "\u{270B}"
+    end
+
+
   end
 
   def rock
-    "\u{270A}"
+    'R'
   end
 
   def paper
-    "\u{270B}"
+    'P'
   end
 
   def scissors
-    "\u{270C}"
+    'S'
   end
 
   def ai_move
     [rock, paper, scissors].sample
   end
 
+
+
   def play
-
-    ai = ai_move
-
-    get_player_input
-
-    compare_fists
-
-    display_results
+    ai=ai_move
+    player_move=@player.get_player_input
+    
+    comparison=compare_fists(player_move,ai)
+    display_results(player_move,ai,comparison)
   end
+
+  def compare_fists(player_choice,ai_choice)
+    if player_choice == 'R'
+      if ai_choice == 'R'
+        'DRAW'
+      elsif ai_choice == 'P'
+        'YOU LOSE'
+      else
+        'YOU WIN'
+      end
+    elsif player_choice == 'P'
+      if ai_choice == 'R'
+        'YOU WIN'
+      elsif ai_choice == 'P'
+        'DRAW'
+      else
+        'YOU LOSE'
+      end
+    else
+      if ai_choice == 'R'
+        'YOU LOSE'
+      elsif ai_choice == 'P'
+        'YOU WIN'
+      else
+        'DRAW'
+      end
+    end
+  end
+
+  def display_results(p,a,comp)
+    puts "\n#######################"
+    puts "Player: #{unicode(p)} VS. AI: #{unicode(a)}"
+    puts "Result: #{comp}"
+    puts "#######################"
+  end
+
 end
+
 
 class Player
   def initialize
+
+  end
+
+  def get_player_input
+    loop do
+     puts "Select Rock, Paper, or Scissors (R, P, S):"
+     choice=gets.chomp.upcase
+     return choice if ['R','P','S'].include?(choice)
+    end
+
   end
 end
 
 
+g=Game.new
+g.play
