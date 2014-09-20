f1a6a0ae4353e3e840bbfc8d3df45eb841bddffa
Andur Carr <carr.andur@gmail.com>
2014-09-19 20:04:27 -0700

    Add basic my_each, my_map, and my_select

--- warmup/warmup.rb
+++ warmup/warmup.rb
@@ -1,1 +1,48 @@
-# Your code here!
+class Array
+
+  def my_each
+    index = 0
+
+    while index <= self.length-1
+      yield(self[index])
+      index += 1
+    end
+
+    self
+  end
+
+  def my_map(proc=nil)
+    duplicate = []
+
+    self.my_each do |x|
+      if block_given?
+        duplicate << yield(x)
+      elsif proc
+
+      else
+
+      end
+    end
+
+    duplicate
+  end
+
+  def my_select
+    selected_array = []
+
+    self.my_each do |x|
+      if yield(x)
+        selected_array << x
+      end
+    end
+
+    selected_array
+  end
+
+end
+
+array = ['Elephant', 'Lion', 'Gazelle']
+
+array.my_each { |x| puts x }
+puts array.my_map { |x| x.upcase }
+puts array.my_select { |x| x == 'Elephant' }
