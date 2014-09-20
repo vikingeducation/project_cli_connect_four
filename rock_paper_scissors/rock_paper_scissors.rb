0a8b7a530076af17f9ebe48c6dd827cc41ce817b
Michael Alexander <alexandermw@gmail.com>
2014-09-19 20:31:54 -0700

    awesome unicode art

--- rock_paper_scissors/rock_paper_scissors.rb
+++ rock_paper_scissors/rock_paper_scissors.rb
@@ -1,1 +1,40 @@
 # Your code here!
+
+class Game
+  def initialize
+  end
+
+  def rock
+    "\u{270A}"
+  end
+
+  def paper
+    "\u{270B}"
+  end
+
+  def scissors
+    "\u{270C}"
+  end
+
+  def ai_move
+    [rock, paper, scissors].sample
+  end
+
+  def play
+
+    ai = ai_move
+
+    get_player_input
+
+    compare_fists
+
+    display_results
+  end
+end
+
+class Player
+  def initialize
+  end
+end
+
+
