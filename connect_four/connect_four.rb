db1bf5767d6d6d53375bc52d339f799079148ddc
Paul Hyman <mail@phyman.com>
2014-09-20 21:25:27 -0500

    Framing Classes

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -7,3 +7,82 @@
 
 #!/usr/bin/ruby
 
+	class ConnectFourGame
+
+		@col_num = 7
+		@row_num = 6
+
+		def initialize(args)
+		
+			@board = Board.new(@col_num,@row_num)
+
+		end
+			
+	end # ConnctFourGame
+		
+	
+	class Board
+
+		##
+		#
+		def render()
+		end 
+
+		##
+		#
+		def get_playfield()
+			# pass the playfield back to the caller
+			return @playfield
+		end
+
+		##
+		#
+		def verify_drop(col_num)
+			ret_val = false
+			if (1..@col_num).include?(col_num) && space?(col_num)
+				ret_val = true
+			end
+			return ret_val
+		end
+
+		##
+		#
+		def space?(col_num)
+			@playfield[col_num].size < row_num
+		end
+		
+		##
+		#
+		def initialize(col_num, row_num)
+			@col_num = col_num
+			@row_num = row_num
+			@playfield = Array.new(@col_num) { Array.new(@row_num) }
+		end
+
+	end # Board
+		
+	class Player
+		
+		##
+		#
+		def drop_piece(col_num)
+			if @board.verify_drop(col_num)
+				@board[col_num] << piece 
+			end
+		end
+		
+		def initialize(player_num = 9, piece = "$", board)
+			@player_num = player_num
+			@piece = piece
+			@board = board
+		end
+
+	end # end Player
+			
+	class AI < Player
+		
+		def initialize(player_num = 9, piece = "$", board)
+		
+		end		
+
+	end # end AI
