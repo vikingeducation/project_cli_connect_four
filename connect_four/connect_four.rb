146e6f2a9d45ca91b03cb95ed9a63767f26aa95f
Daniel Au <dakmau@gmail.com>
2015-01-23 19:48:24 -0500

    Psuedocode for connect_four

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,1 +1,36 @@
-# Your code here!
+ConnectFour object
+
+end
+
+Board object: Array of arrays
+    [[x,x,x,nil,nil,nil],      <= one column 
+     [o,x,nil,nil,nil,nil]]    <= second column
+     
+     
+     board[column] << "o"     <= Move adding 'o' to the "top" of that column
+
+methods:
+
+#Graphics
+render
+
+#Modifying the board
+validate column_is_not_full
+insert_coin_into_column
+
+# Winning conditions
+check vertical
+check horizontal
+check diagonal
+end
+
+Player object
+
+end
+
+AI object
+
+end
+
+
+
