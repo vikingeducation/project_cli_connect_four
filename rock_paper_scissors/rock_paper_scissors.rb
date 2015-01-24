f5c2744fbb635c8c3f038be9f78d59902a04d88f
Schwad <nicholas.schwaderer@gmail.com>
2015-01-24 19:00:26 +0000

    play method

--- rock_paper_scissors/rock_paper_scissors.rb
+++ rock_paper_scissors/rock_paper_scissors.rb
@@ -44,21 +44,43 @@ class Game
 	def one_player_play
 		user_1=Player.new("computer")
 		user_2=Player.new("human")
-
+		win(user_1.guess,user_2.guess)
 	end
 
 	def two_player_play
 		user_1=Player.new("human")
 		user_2=Player.new("human")
+		win(user_1.guess,user_2.guess)
 	end
 
 	def play
 		welcome
-		num_human
-		one_player_play if @humans=1
-		two_player_play if @humans=2
-
+		loop do
+			num_human
+			one_player_play if @humans=1
+			two_player_play if @humans=2
+			puts "Thank you for playing Rock Paper Scissors!"
+			puts "Here is the score:\nPlayer_1(#{user_1.role}) - #{user_1.score}\nPlayer_2(#{user_2.role}) - #{user_2.score}"
+			print "Would you like to play again (y/n)?"
+			answer = gets.chomp.to_s.downcase
+			until validate_again?(answer) do 
+				print "invalid selection! Please re-enter: (y/n)"
+				answer = gets.chomp.to_s.downcase
+			end
+			if !again?(answer)
+				exit
+			end
+		end
 	end
+
+	def again?(answer)
+		answer == "y" ? true : false
+	end
+
+	def validate_again?(answer)
+		%w("y" "n").include?(answer)
+	end
+
 
 	def validate_human(human)
 		[1,2].include?(human) ? true : false
