a90f123102b7e900d774e4294d9f0daa6e27e6f3
Daniel Au <dakmau@gmail.com>
2015-01-23 19:10:08 -0500

    One-player game works great

--- rock_paper_scissors/rock_paper_scissors.rb
+++ rock_paper_scissors/rock_paper_scissors.rb
@@ -1,11 +1,11 @@
 class RockPaperScissors
-  attr_reader :player_quits
+  attr_reader :player_quits, :player, :ai
   attr_accessor :player_one_wins
 
 # Set up the game initially
   def initialize
     @player_quits = false
-    @player_one_wins
+    @player_one_wins = false
     @player = Player.new
     @ai = AI.new
     play
@@ -14,25 +14,48 @@ class RockPaperScissors
   def play
     until player_quits
       player_move = player.ask_for_player_choice
-      ai_move = ask_for_ai_move
+      ai_move = ai.ask_for_ai_move
       if is_draw?(player_move,ai_move)
         puts "It's a draw"
       else
         pick_winner(player_move,ai_move)
+        display_winner_message ai_move
       end
-
+      ask_player_to_continue
     end
   end
 
   def pick_winner(move1,move2)
     case
-    when move1 == "R" && move2 == "P"
-      player_one_wins == false
     when move1 == "R" && move2 == "S"
+      self.player_one_wins = true
+    when move1 == "S" && move2 == "P"
+      self.player_one_wins = true
+    when move1 == "P" && move2 == "R"
+      self.player_one_wins = true
+    else
+      self.player_one_wins = false
+    end
   end
 
   def is_draw?(move1,move2)
     move1 == move2
+  end
+
+
+  def ask_player_to_continue
+    puts "Enter 'q' to quit, or anything else to play again: "
+    quit_prompt = gets.chomp.upcase
+    @player_quits = (quit_prompt == 'Q')
+  end
+
+  def display_winner_message player_two_move
+    puts "Player two chose #{player_two_move}"
+    if player_one_wins
+      puts "Player one wins!"
+    else
+      puts "Player two wins!"
+    end
   end
 # Compare the moves to declare a winner
 # Ask if player wants to play again.
