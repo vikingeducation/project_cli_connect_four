367c9697e771b9d4f6233dbe467cdcf30919c168
Schwad <nicholas.schwaderer@gmail.com>
2015-01-24 00:07:29 +0000

    warmup

--- warmup/warmup.rb
+++ warmup/warmup.rb
@@ -7,4 +7,18 @@ class Array
       end 
       arr
   end
+
+  def my_map(arr,act)
+      i = 0
+      while i < arr.size
+          yield(act(arr[i]))
+          i+=1
+      end 
+      arr
+  end
+
+  def my_select(arr,criteria)
+      arr.my_each { |element|  element.criteria ? : yield(element) nil}
+  end 
+
 end
