4291f69a91f67430cbc2e3da9ec738c821c34a4f
telwell <me@trevorelwell.com>
2015-04-04 10:14:41 -0400

    Added my_each method

--- warmup/warmup.rb
+++ warmup/warmup.rb
@@ -1,1 +1,17 @@
-# Your code here!
+# Create my_each method, add to Array class
+class Array
+	def my_each
+		0.upto(self.length-1) do |index|
+			# I want to leave the functionality
+			# up to the user if the give a block
+			if block_given?
+				yield(self[index])
+			else
+				# Otherwise just output each item
+				puts self[index]
+			end
+		end
+		self
+	end
+end
+
