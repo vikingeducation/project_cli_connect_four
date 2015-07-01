1669d94254d82bfc6551aa04a88b4dce5f5ad1b6
Nick Sarlo <nicholas.l.sarlo@gmail.com>
2015-07-01 13:58:18 -0400

    added pseudo code

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,1 +1,11 @@
-# Your code here!
+Initialize game [ConnectFour]
+Create empty board [Board]
+Create Player [Player]
+
+loop until game conditions met
+	Render board [Board]
+	Get player move [Player]
+	Add move to board [Board]
+	Check game end conditions [Board]
+
+
