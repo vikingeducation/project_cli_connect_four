6f37ce91577009f00de50639be663b3a1cd5df0d
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-01 15:57:02 -0700

    basic game funcitonality

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -28,17 +28,38 @@ class ConnectFour
   def play
     # print_instructions
 
-    @board.render
 
-    current_player = 1
+    current_player = @player1
+    @num = 1
+
     loop do
-      @board.move(@player1.get_move, 1)
-
-      system "clear"
       @board.render
-      @board.victory?
+      puts "Player #{@num} turn"
+      @board.move(current_player.get_move, @num)
+      if @board.victory?
+        @board.render
+        puts "Player #{@num} Wins!"
+        break
+      end
+      switch_player(current_player)
+    end
 
+  end
 
+  def switch_player(current_player)
+
+    if @num == 1
+
+      @num = 2
+    else
+      @num = 1
+
+    end
+
+    if current_player == @player1
+      current_player = @player2
+    else
+      current_player = @player1
     end
 
   end
@@ -59,8 +80,13 @@ class Board
 
   def render
 
-    display = @game #.transpose
+    display = @game.transpose
     display.each {|i| p i }
+    puts " --------------------"
+    7.times { |i| print "-"; print (i + 1 ); print ("-")}
+    puts
+    
+    puts
 
   end
 
@@ -79,15 +105,14 @@ class Board
 
   def victory?
 
-    #vert_victory? || horz_victory? || diag_victory?
-    diag_victory?
+    vert_victory? || horz_victory? || diag_victory?
       
 
   end
 
   def horz_victory?
 
-    puts "horz_test"
+
 
     @game.each do |row|
 
@@ -101,7 +126,7 @@ class Board
            row[i+2] == row[i+3] &&
            row[i]   != 0
 
-          puts "hi"
+
           return true
   
 
@@ -118,9 +143,7 @@ class Board
 
   def vert_victory?
 
-    puts "vert test"
-
-
+    
   
     @game.transpose.each do |row|
 
@@ -134,7 +157,6 @@ class Board
            row[i+2] == row[i+3] &&
            row[i]   != 0
 
-          puts "win"
           return true
         
         end
@@ -150,20 +172,50 @@ class Board
 
   def diag_victory?
 
-    i = 0
-    if @game[i][i]         == @game[i + 1][i + 1] &&
-       @game[i + 1][i + 1] == @game[i + 2][i + 2] &&
-       @game[i + 2][i + 2] == @game[i + 3][i + 3] &&
-       @game[i][i]         != 0
 
-      puts "diag victory!"
-      return true
+    i = 0
+    4.times do |vert|
+      3.times do |horz|
+
+        if @game[vert + i]    [horz + i]     == @game[vert + i + 1][horz + i + 1] &&
+           @game[vert + i + 1][horz + i + 1] == @game[vert + i + 2][horz + i + 2] &&
+           @game[vert + i + 2][horz + i + 2] == @game[vert + i + 3][horz + i + 3] &&
+           @game[vert + i]    [horz+ i]      != 0
+
+
+          puts "diag victory!"
+          i+1
+          return true
+        end
+
+
+      end
+
+    end
+
+    i = 0
+
+
+    4.times do |vert|
+      3.times do |horz|
+
+        if @game.reverse[vert + i]    [horz + i]     == @game.reverse[vert + i + 1][horz + i + 1] &&
+           @game.reverse[vert + i + 1][horz + i + 1] == @game.reverse[vert + i + 2][horz + i + 2] &&
+           @game.reverse[vert + i + 2][horz + i + 2] == @game.reverse[vert + i + 3][horz + i + 3] &&
+           @game.reverse[vert + i]    [horz+ i]      != 0
+
+
+          puts "diag2 victory!"
+          i+1
+          return true
+        end
+
+      end
+
     end
 
     return false
-  end
-
-  
+  end  
 
 end
 
