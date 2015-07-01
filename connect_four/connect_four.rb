17eb7b1c0c1118dbc0a2db190737f7874d6829ec
Joseph Lozano <lozanojoseph@gmail.com>
2015-07-01 11:25:03 -0700

    Board.render method

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,3 +1,5 @@
+=begin
+
 Initialize game [ConnectFour]
 Create empty board [Board]
 Create Player [Player]
@@ -8,4 +10,86 @@ loop until game conditions met
 	Add move to board [Board]
 	Check game end conditions [Board]
 
+=end
 
+
+class ConnectFour
+
+  def initialize
+
+    @player1 = Human.new
+    @player2 = Human.new 
+
+    @board = Board.new
+
+  end
+
+
+  def play
+    # print_instructions
+
+    @board.render
+
+
+
+
+  end
+
+end
+
+
+
+class Board
+
+  def initialize
+
+    @game = Array.new(6) { Array.new(7) { 0 }  }
+
+    # @game = [
+    #           [0,0,0,0,0,0,0],
+    #           [0,0,0,0,0,0,0],
+    #           [0,0,0,0,0,0,0],
+    #           [0,0,0,0,0,0,0],
+    #           [0,0,0,0,0,0,0],
+    #           [0,0,0,0,0,0,0],
+    #           [0,0,0,0,0,0,0]
+    #         ]
+
+
+  end
+
+  def render
+
+    @game.each {|i| p i }
+
+  end
+
+end
+
+
+
+class Player
+
+
+
+
+end
+
+
+
+class Human < Player
+
+  def get_move
+
+    puts "Some intructions"
+    #
+    move = gets.chomp
+
+  end
+
+end
+
+
+c4 = ConnectFour.new
+
+c4.play
