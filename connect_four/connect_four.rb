1d6e3e90944ce738e8a2d890608bfdec4d91742b
ayva <olga.ayva@gmail.com>
2015-07-01 11:25:31 -0700

    render and move

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,22 +1,25 @@
 # Your code here!
 
 class Game
-
+  
   def initialize
-    @board = Board.new
+    
+
     start_game
   end
   #initialize players and board
 
   #start game loop
   def start_game
-    player1 = Player.new
+    @board = Board.new.build_board
+    puts "Game started"
+    player1 = Player.new 
     player2 = Player.new
-    @board.render
+    
     loop do
-      player1.move
+      player1.move(@board)
       @board.render #include move check and display
-      player2.move
+      player2.move(@board)
       @board.render
     end
   end
@@ -30,13 +33,20 @@ end
 class Player
   #pass instance into game
   #make a move
-  def get_input
-    puts "Which column do you want to put your piece in?"
-    input = gets.chomp
+  def initialize
+    @board
   end
 
-  def move
-    get_input
+  def get_input
+    puts "Which column do you want to put your piece in? (from column from 1 to 7)"
+    input = gets.chomp.to_i
+  end
+
+  def move(board_current)
+    move=get_input
+    puts move
+    board_current[5][move] = "-"
+    
   end
   # =>valid move?
   def move_valid?
@@ -46,19 +56,33 @@ class Player
 end
 
 class Board
+  attr_accessor :field
   #pass instance into game
   #check move in board
   #render
   def initialize
-
+    @field=[]
   end
 
   def render
-
+    for i in (0..5) #rows   
+      for j in (0..6) #columns  
+        print @field[i][j]
+      end
+      puts
+    end
   end
 
   def build_board
     #build board
+    for i in (0..5) #rows
+      @field<<[]
+      for j in (0..6) #columns
+        @field[i][j]="0"
+        print @field[i][j]
+      end
+      puts
+    end
   end
 
 
