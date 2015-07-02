6f7bdbc0c4613cec420e8f4b2effbd60b192565d
michelletoussaint <toussaint.michelle@gmail.com>
2015-07-01 20:28:01 -0400

    created logic for AI

--- connect_four/temp.rb
+++ connect_four/temp.rb
@@ -19,10 +19,38 @@
 
 class ComputerAi
   def intialize (gameboard)
-    @gameboard = gameboard
+    @current_board = gameboard
+  end
+
+  def max_block
+    
+    7.times do |column|
+      bottom_row = bottom?(column)
+      if bottom == false
+        pieces << 0
+      else
+        pieces << check_continuous_pieces (bottom_row, column)
+      end
+    end
+
+    #pieces [2, 3, 0, 1, 1, 0, 0]
+    return pieces.index(pieces.max)
+
+  end
+
+  def bottom?(column)
+
+    5.downto(0) do |row|
+      if @current_board[row][column] == "-"
+        return row
+      end
+    end
+
+    return false
+
   end
   
-  def computer_check_three(row, column)
+  def check_continuous_pieces(row, column)
     piece = @current_board[row][column]
     counter = 1
     #counting in down on the board (not array number) direction
