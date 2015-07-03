9e7ae5ec87eec8ffc5273593d65f2c01f1d61bfe
alokp <alokpra@gmail.com>
2015-07-02 18:39:27 -0700

    refactored tests

--- connect_four/spec/player_spec.rb
+++ connect_four/spec/player_spec.rb
@@ -4,7 +4,7 @@ require 'board'
 describe Human do
   describe '#move' do
     let(:player){Human.new}
-    it 'move returns decreased by 1 input' do
+    it 'gets human input and decrements by 1' do
       allow(player).to receive(:gets).and_return("4")
       expect(player.move).to be(3)
     end
@@ -15,20 +15,20 @@ describe AI do
   let(:board){Board.new}
   let(:ai){AI.new(board, 1)}
   describe '#move' do
-    it 'AI should make a move if possible' do
+    specify 'AI should make a move if possible' do
       expect((ai.move <= 5) && (ai.move >= 0)).to be_truthy
     end
 
     it 'AI makes a winning move if possible' do
       3.times do
-      board.move([0,1])
+        board.move([0,1])
       end
       expect(ai.move).to be(0)
     end
 
     it 'AI makes a blocking move if possible' do
       3.times do
-      board.move([5,0])
+        board.move([5,0])
       end
       expect(ai.move).to be(5)
     end
