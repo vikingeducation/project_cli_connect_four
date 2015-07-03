495217dbb90b834917116309c8c3dc09d679ffd2
Garrett <garrettweinert@gmail.com>
2015-07-02 21:22:17 -0700

    ConnectFour before refactor.

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -4,7 +4,6 @@ class ConnectFour
     @board = GameBoard.new
     @player1 = Player.new("Player 1", "x", @board)
     @player2 = Player.new("Player 2", "o", @board)
-
   end
 
   def play 
@@ -12,8 +11,11 @@ class ConnectFour
     @board.render
     input = ""
     current_player = @player1
-    until player_quit?(input) || @board.win_condition?
+    until @board.win_condition?
       input = current_player.get_input
+      if player_quit?(input)
+        break
+      end
       @board.update_board(input, current_player.piece)
       @board.render
       if @board.win_condition?
@@ -81,9 +83,8 @@ class GameBoard
   end
 
   def update_board(column_input, piece)
-    
-    @game_board_arr[column_input].push(piece)
-    
+
+      @game_board_arr[column_input].push(piece)
     
   end
 
@@ -135,7 +136,7 @@ class GameBoard
   end
 
   def horizontal_win?
-    return_bool = false
+
     test_array = []
     row_index = 0
     row = 0
@@ -146,22 +147,19 @@ class GameBoard
       end
 
       if winning_array?(test_array)
-        return_bool =  true
+        return true
       else
-        return_bool = false
+        return false
       end
-
       row_index +=1
       row = 0
     end
-    return_bool
   end
 
   def diagonal_win?
      
     4.times do |column|
       3.times do |row|
-
         if @game_board_arr[column ]    [row ] != nil && 
           (@game_board_arr[column]    [row   ] == 
            @game_board_arr[column + 1][row + 1] &&
@@ -169,17 +167,11 @@ class GameBoard
            @game_board_arr[column + 2][row + 2] &&
            @game_board_arr[column + 2][row + 2] == 
            @game_board_arr[column + 3][row + 3])
-           
+  
           return true
         end
-
-
       end
-
     end
-
-    i = 0
-
 
     4.times do |column|
       3.times do |row|
@@ -194,11 +186,8 @@ class GameBoard
 
           return true
         end
-
       end
-
     end
-
     return false
   end
 
