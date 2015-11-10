16b9934fb3387e572e3a301b906986422f0dac29
Sia <siakaramalegos@gmail.com>
2015-11-09 18:58:05 -0600

    Adds diagonals check

--- connect_four/connect_four_board.rb
+++ connect_four/connect_four_board.rb
@@ -58,8 +58,7 @@ class Board
   private
 
   def winning_diagonal?(symbol)
-    # TODO: create
-    false
+    all_diagonals.any? {|diag| diag.include?(symbol * 4)}
   end
 
   def winning_horizontal?(symbol)
@@ -87,6 +86,34 @@ class Board
       end
     end
     horizontals
+  end
+
+  def all_diagonals
+    diagonals = []
+    valid_col_rows = [
+      [ [0,2], [1,3], [2,4], [3,5] ],
+      [ [0,1], [1,2], [2,3], [3,4], [4,5] ],
+      [ [0,0], [1,1], [2,2], [3,3], [4,4], [5,5] ],
+      [ [1,0], [2,1], [3,2], [4,3], [5,4], [6,5] ],
+      [ [2,0], [3,1], [4,2], [5,3], [6,4] ],
+      [ [3,0], [4,1], [5,2], [6,3] ],
+
+      [ [0,3], [1,2], [2,1], [3,0] ],
+      [ [0,4], [1,3], [2,2], [3,1], [4,0] ],
+      [ [0,5], [1,4], [2,3], [3,2], [4,1], [5,0] ],
+      [ [1,5], [2,4], [3,3], [4,2], [5,1], [6,0] ],
+      [ [2,5], [3,4], [4,3], [5,2], [6,1] ],
+      [ [3,5], [4,4], [5,3], [6,2] ],
+    ]
+
+    valid_col_rows.each do |diag|
+      diagonal = ''
+      diag.each do |coords|
+        diagonal += @columns[coords.first][coords.last]
+      end
+      diagonals << diagonal
+    end
+    diagonals
   end
 
 end
