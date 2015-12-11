362a99ec01e541f12dbc6b236d2448418affc79f
Steven Chang <prime_pork@hotmail.com>
2015-12-11 17:58:51 +1000

    Refactored board class

--- connect_four/board.rb
+++ connect_four/board.rb
@@ -37,30 +37,27 @@ class Board
   def winner?(piece)
     @answer = []
     4.times {@answer << piece}
-    @grid.each_with_index do |column, index|
+    @grid.each_with_index do |column, column_number|
       return true if vertical_four?(column)
-      if index < 4
-        return true if horizontal_win?(index)
-        return true if diagonal_win?(index)
+      if column_number < 4
+        return true if horizontal_win?(column_number)
+        return true if diagonal_win?(column_number)
       end
     end
     false
   end
 
-  # How do you determine a winning move?
-  # by seeing any four in a rows and there's a gap to fill with your piece.
   def winning_move?(piece)
     @answer = []
     4.times {@answer << piece}
-    @grid.each_with_index do |column, index|
+    @grid.each_with_index do |column, column_number|
       if vertical_move?(column)
-        @winning_move = index + 1
-        return true 
+        @winning_move = column_number + 1
+        return true
       end
-      if index < 4
-        if horizontal_move?(index)
-          @winning_move = winning_horizontal_position(index) + 1
-          puts @winning_move
+      if column_number < 4
+        if horizontal_move?(column_number)
+          @winning_move = winning_horizontal_position(column_number) + 1
           return true
         end
       end
@@ -70,32 +67,26 @@ class Board
 
   private
 
-  def winning_horizontal_position(index)
-    position = nil
-    @grid[index].each_with_index do |value, spot|
-      if valid_horizontal_finisher?(index, spot)
-        @horizontal_four.each_with_index do |piece, location|
-          if ((piece == " ") && (spot == 0 || (@grid[index + location][spot - 1] != " ")))
-            return position = index + location
+  def winning_horizontal_position(column)
+    @grid[column].each_with_index do |value, row|
+      if valid_horizontal_finisher?
+        @horizontal_four.each_with_index do |piece, piece_location|
+          if ((piece == " ") && (row == 0 || (@grid[column + piece_location][row - 1] != " ")))
+            return column + piece_location
           end
         end
       end
     end
   end
 
-  def valid_horizontal_finisher?(index, spot)
-    if @horizontal_four - @answer == [" "]
-      return true
-    end
-    false
+  def valid_horizontal_finisher?
+    @horizontal_four - @answer == [" "]
   end
 
   def horizontal_move?(column)
-    @grid[column].each_with_index do |spot, index|
-      @horizontal_four = [@grid[column][index], @grid[column + 1][index], @grid[column + 2][index], @grid[column + 3][index]]
-      if valid_horizontal_finisher?(column, index)
-        return true
-      end
+    @grid[column].each_with_index do |value, row|
+      @horizontal_four = [@grid[column][row], @grid[column + 1][row], @grid[column + 2][row], @grid[column + 3][row]]
+        return true if valid_horizontal_finisher?
     end
     false
   end
@@ -111,23 +102,23 @@ class Board
     false
   end
 
-  def diagonal_win?(index)
+  def diagonal_win?(column)
     row = 0
     while row < 6
       if row <= 2
-        return true if [@grid[index][row], @grid[index+1][row + 1], @grid[index + 2][row + 2], @grid[index+3][row + 3]] == @answer
+        return true if [@grid[column][row], @grid[column + 1][row + 1], @grid[column + 2][row + 2], @grid[column + 3][row + 3]] == @answer
       else
-        return true if [@grid[index][row], @grid[index+1][row - 1], @grid[index+2][row - 2], @grid[index+3][row - 3]] == @answer
+        return true if [@grid[column][row], @grid[column + 1][row - 1], @grid[column + 2][row - 2], @grid[column + 3][row - 3]] == @answer
       end
       row += 1
     end
     false
   end
 
-  def horizontal_win?(index)
+  def horizontal_win?(column)
     row = 0
     while row < 6
-      return true if [@grid[index][row], @grid[index+1][row], @grid[index+2][row], @grid[index+3][row]] == @answer
+      return true if [@grid[column][row], @grid[column + 1][row], @grid[column + 2][row], @grid[column + 3][row]] == @answer
       row += 1
     end
     false
