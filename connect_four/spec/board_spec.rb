5189849e1f193945b51cc313f3229f416af55601
Steven Chang <prime_pork@hotmail.com>
2015-12-20 18:06:56 +1000

    board initialize method tests have come back good

--- connect_four/spec/board_spec.rb
+++ connect_four/spec/board_spec.rb
@@ -2,5 +2,21 @@ require_relative 'spec_helper'
 require_relative '../lib/board'
 
 describe Board do
+  let(:board){ Board.new }
 
+  describe '#initialize' do
+
+    it 'should give you a Board' do
+      expect(board).to be_a(Board)
+    end
+
+    it 'should set up seven arrays with 6 spots in them each' do
+      expect(board.grid).to eq([[' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' '], [' ', ' ', ' ', ' ', ' ', ' ']])
+    end
+
+    it 'should have a winning move which is nil' do
+      expect(board.winning_move).to eq(nil)
+    end
+
+  end
 end
