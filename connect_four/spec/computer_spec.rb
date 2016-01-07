36fde5139e49784e1ae3f1382fb0262238a81a73
Kelsey James <kelseybjames@gmail.com>
2016-01-07 12:00:10 -0800

    More tests, having infinite loop problems

--- connect_four/spec/computer_spec.rb
+++ connect_four/spec/computer_spec.rb
@@ -1,3 +1,4 @@
+require 'player'
 require 'computer'
 
 describe Computer do
@@ -5,7 +6,24 @@ describe Computer do
   let ( :computer ) { Computer.new("green") }
   let ( :computer_no_color ) { Computer.new }
 
-  
+  describe "#initialize" do
+    it "initializes properly" do
+      expect(computer).to be_an_instance_of(Computer)
+    end
 
+    it "initializes properly with color" do
+      expect(computer.color).to eq("green")
+    end
 
+    it "doesn't initialize without color" do
+      expect{computer_no_color}.to raise_error(ArgumentError)
+    end
   end
+
+  describe "#player_move" do
+    it "returns a column between 0 and 6" do
+      expect(computer.player_move).to be_between(0, 6)
+    end
+  end
+
+end
--- connect_four/spec/notes.rb
+++ connect_four/spec/notes.rb
@@ -100,7 +100,7 @@ Human
     make sure we get an integer back
 
     try invalid input, make sure we do not get an integer back
-
+      HAVING PROBLEM WITH INFINITE LOOP
 
 ======================================  
 Player
