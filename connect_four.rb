a9c75ef14c489838cc72a885f7d0e80cc7ed1112
Stephen Mayeux <stephenmayeux@gmail.com>
2016-04-30 15:19:22 +0900

    input validation for human players

--- connect_four.rb
+++ connect_four.rb
@@ -1,41 +1,82 @@
 class Game
 
   def initialize(num_of_players=1)
-    @player1 = Player.new
+    @player1 = Player.new("X")
     @board = Board.new
 
     if num_of_players == 1
-      @player2 = Computer.new
+      @player2 = Computer.new("O")
     else
-      @player2 = Player.new
+      @player2 = Player.new("O")
     end
 
     @current_player = @player1
   end
 
   def play
-    until game.over?
+    until over?
 
       @board.render
 
-      #@current_player.get_move
+      check_move(@current_player.get_move)
 
       #check_win
 
       #check_full_board
 
-      #switch_player
+      switch_player
 
     end
+  end
+
+  def check_move(move)
+    puts move
+    if move > 7 || move < 1
+      puts "Invalid move! It is out of bounds."
+      check_move(@current_player.get_move)
+    elsif @board.columns[move].length == 7
+      puts "Invalid move! Column #{move} is full."
+      check_move(@current_player.get_move)
+    else
+      @board.columns[move] << @current_player.piece
+    end
+  end
+
+  def switch_player
+    if @current_player == @player1
+      @current_player = @player2
+    else
+      @current_player = @player1
+    end
+  end
+
+  def over?
+    false
   end
 
 end
 
 class Player
 
+  attr_reader :piece
+
+  def initialize(piece)
+    @piece = piece
+  end
+
+  def get_move
+    puts "Enter 1-6 to make a move."
+    move = gets.chomp
+    return move.to_i
+  end
+
 end
 
 class Computer
+
+  def initialize(piece)
+    @piece = piece
+  end
 
 end
 
