032637bee24d79f9491e79e4a0809dae551958ed
Stephen Mayeux <stephenmayeux@gmail.com>
2016-05-05 07:58:02 +0900

    check for empty board

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,5 +1,29 @@
 require 'connect_four'
 
+describe "Board" do
+
+  let(:board){ Board.new }
+
+  describe "#new board" do
+
+    it "has 6 empty arrays" do
+      def empty_board?(columns)
+        i = 1
+        while i <= board.columns.length
+          if board.columns[i].length > 0
+            return false
+          end
+          i += 1
+        end
+        return true
+      end
+      expect(empty_board?(board.columns)).to be(true)
+    end
+
+  end
+
+end
+
 describe "Game Class" do
 
   let(:game_1_player){ Game.new(1) }
