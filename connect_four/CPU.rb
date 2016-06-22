1152ec86f2b3ce8294a0c991b898b49b2a10956b
Joe Bernardi <joebernardi@hotmail.com>
2016-06-21 22:10:10 -0500

    add row wins for CPU...needs refactoring

--- connect_four/CPU.rb
+++ connect_four/CPU.rb
@@ -5,13 +5,7 @@ class CPU < Player
 		# the CPU will be generated only if 1 player is selected
 		# the CPU will be able to evaluate the board
 	def evaluate_board
-		# want to place next to own piece
-		# or to win
-		#three_pieces = @piece.to_s * 3
-		#row_arr = @board.board_as_string_row
-		#binding.pry
-		# check for 3 in a row of its color
-		# check rows
+
 		row = @board.board.join
 		column = @board.board_as_string_col
 
@@ -19,20 +13,79 @@ class CPU < Player
 
 			return move_for_column_win( column )
 
+		elsif row.include?( @piece.to_s * 2)
+
+			return move_for_row_win( row )
+
 		else
+
 			return rand(1..7)
+
 		end
 
+
 		diag = @board.board_as_string_diagonal
-		# check columns
-		# check diagonal
-		# if no options
-		# place where 1 or 2 of CPU color
-		# eval the entire array?
-		# check for 3 of the CPU's piece
-		#return column
+
 
   end
+
+
+  def move_for_row_win( string )
+
+  	# generate 6 rows as an array
+  	row_arr = string.split("]")
+  	bobb = "BOBB"
+  	bbob = "BBOB"
+  	bb   = "BB"
+
+  	row_arr.each_with_index do | x, i |
+
+  		if x.include?( @piece.to_s * 3 )
+  			# then need to go through that row to determine where the winning combo is based on the index = column
+  			column = x.index( @piece.to_s * 3 )
+  			if x[ column - 1 ] == "O"
+
+  				# return the column before the BBB
+  				return column - 1
+
+  			else
+
+  				# return column after the BBB
+  				return column + 3
+
+  			end
+  		elsif x.include?( bbob )
+
+  			return ( x.index( bbob ) + 2 )
+
+  		elsif x.include?( bobb )
+
+  			return ( x.index( bobb ) + 1 )
+
+  		elsif x.include?( bb )
+
+  			column = x.index( bb )
+  			if x[ column - 1 ] == "O"
+
+  				# return the column before the BBB
+  				return column - 1
+
+  			else
+
+  				# return column after the BBB
+  				return column + 2
+
+  			end
+
+      end
+
+    end
+
+
+  end
+
+
+
 
   def move_for_column_win( string )
 
