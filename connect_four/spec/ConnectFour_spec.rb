740aea9044fef6ee3874475b1c4e15cfc1bf6185
Joe Bernardi <joebernardi@hotmail.com>
2016-06-25 13:04:57 -0500

    add pending tests

--- connect_four/spec/ConnectFour_spec.rb
+++ connect_four/spec/ConnectFour_spec.rb
@@ -1,1 +1,14 @@
 require 'spec_helper'
+require 'ConnectFour'
+
+
+describe '.ConnectFour' do
+
+	it "pending creates a new object"
+	it "creates a Player"
+	it "creates a computer"
+	it "runs the num_players"
+	it "returns a num to generate players/cpu"
+
+
+end
