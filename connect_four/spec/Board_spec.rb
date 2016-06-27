85f0b60a1d84989a36f7031c318480a885d606cc
Joe Bernardi <joebernardi@hotmail.com>
2016-06-26 21:53:36 -0500

    add init test for board

--- connect_four/spec/Board_spec.rb
+++ connect_four/spec/Board_spec.rb
@@ -1,1 +1,31 @@
 require 'spec_helper'
+require 'Board'
+
+
+describe '.Board' do
+
+	let( :board ) { Board.new }
+	let( :board_array ) { [
+						 "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-"
+	 				 ] }
+
+	describe '#initialize' do
+
+		it "should create a @board from the default state" do
+
+			expect(board.board).to eq( board_array )
+
+		end
+
+
+	end
+
+
+
+
+end
