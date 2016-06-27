1de808867f16f9af7302e7d1649e6a5e43d928fd
Joe Bernardi <joebernardi@hotmail.com>
2016-06-27 14:55:07 -0500

    add board tests for draw, full columm

--- connect_four/spec/Board_spec.rb
+++ connect_four/spec/Board_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require 'Board'
-
+require 'pry'
 
 describe '.Board' do
 
@@ -13,6 +13,9 @@ describe '.Board' do
 	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
 	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-"
 	 				 ] }
+
+
+
 
 	describe '#initialize' do
 
@@ -38,13 +41,119 @@ describe '.Board' do
 	end
 
 
-	describe "#four_in_a_row"
+	describe '#board_as_string_row' do
 
-		it "should return true if there is 4 in a row"
+		it "should return the board rows as one string" do
+
+			string_row = board.board.join
+
+			expect( board.board_as_string_row ).to eq( string_row )
+
+		end
+
+	end
+
+	describe '#four_in_a_row' do
+
+		it 'should return false if there is not four in a row' do
+
+			board.board = [
+						 "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-"
+	 				 ]
+
+			expect( board.four_in_a_row( :B )).to be false
+
+		end
+
+
+
+
+		it 'should return true if there is 4 in a row' do
+
+			board.board = [
+						 "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "O", "O", "O", "O", "O", "O", "O", "-",
+	 				   "-", "B", "B", "B", "B", "O", "O", "O", "-"
+	 				 ]
+
+			expect( board.four_in_a_row( :B )).to be true
+
+		end
 
 	end
 
 
+
+
+	describe '#draw?' do
+
+		it 'should return true if all positions are filled' do
+
+			board.board = [
+					 "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "B", "B", "B", "B", "R", "R", "R", "-"
+ 				 ]
+
+ 			expect( board.draw?).to be true
+
+ 		end
+
+ 		it 'should return false if there are still open positions' do
+
+ 			board.board = [
+					 "-", "O", "R", "R", "R", "O", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "B", "B", "B", "B", "R", "R", "R", "-"
+ 				 ]
+
+ 			expect( board.draw?).to be false
+
+ 		end
+
+ 	end #/.draw?
+
+
+ 	describe '#column_has_room?' do
+
+ 		it 'should return true if the top row has O ' do
+
+ 			board.board = board_array
+
+ 			expect( board.column_has_room?( 1 )).to be true
+
+ 		end
+
+ 		it 'should return false if the top row as R or B' do
+
+ 			board.board = [
+					 "-", "O", "R", "R", "R", "O", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "R", "R", "R", "R", "R", "R", "R", "-",
+ 				   "-", "B", "B", "B", "B", "R", "R", "R", "-"
+ 				 ]
+
+ 			expect( board.column_has_room?( 2 )).to be false
+
+ 		end
+
+ 	end
 
 
 end
