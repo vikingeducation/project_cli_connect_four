a3e4fe106bff0778d2bb9e0f320be9068e936162
Conor Fischer <conor.fischer@gmail.com>
2016-07-06 17:26:42 -0500

    Add board initialization

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,7 @@
 class Board
-	def initialize
+	attr_reader :board_array
+
+	def initialize( board_array = nil)
+		@board_array = board_array || Array.new(6) { Array.new(3) }
 	end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,4 +4,10 @@ describe Board do
   	it "is a board" do
 		expect(subject).to be_a(Board)
 	end
+
+	it "accepts a board array upon creation" do
+		expect do
+			Board.new(Array.new(6) { Array.new(3) })
+		end.not_to raise_error
+	end
 end
