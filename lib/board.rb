cea9060c684b6f5ff616f90ec0b0d9a081fa3407
Conor Fischer <conor.fischer@gmail.com>
2016-07-06 17:53:07 -0500

    Add Board render

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,16 @@ class Board
 	attr_reader :board_array
 
 	def initialize( board_array = nil)
-		@board_array = board_array || Array.new(6) { Array.new(3) }
+		@board_array = board_array || Array.new(6) { Array.new(7) }
+	end
+
+	def render
+		puts
+		@board_array.each do |row|
+			row.each do |column|
+				column.nil? ? print("-") : print(column.to_s)
+			end
+			puts
+		end
 	end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,4 +10,30 @@ describe Board do
 			Board.new(Array.new(6) { Array.new(3) })
 		end.not_to raise_error
 	end
+
+	describe "#render" do
+		it "displays empty board at initialization" do
+			expect{subject.render}.to output("#{"\n-------"*6}\n").to_stdout
+		end
+
+		it "displays board in progress" do
+			progress_board = Board.new([[nil,nil,nil,nil,nil,nil,nil],
+										[nil,nil,nil,nil,nil,nil,nil],
+										[nil,nil,nil,nil,nil,nil,nil],
+										[nil,nil,nil,nil,"X",nil,nil],
+										[nil,nil,nil,"O","X",nil,nil],
+										["O","O","X","O","X",nil,nil]])
+			expect{progress_board.render}.to output("\n-------\n-------\n-------\n----X--\n---OX--\nOOXOX--\n").to_stdout
+		end
+
+		it "displays board thats won" do
+			won_board = Board.new( [[nil,nil,nil,nil,nil,nil,nil],
+									[nil,nil,nil,nil,nil,nil,nil],
+									[nil,nil,nil,nil,"X",nil,nil],
+									[nil,nil,nil,nil,"X",nil,nil],
+									[nil,nil,nil,"O","X",nil,nil],
+									["O","O","X","O","X",nil,nil]])
+			expect{won_board.render}.to output("\n-------\n-------\n----X--\n----X--\n---OX--\nOOXOX--\n").to_stdout
+		end
+	end
 end
