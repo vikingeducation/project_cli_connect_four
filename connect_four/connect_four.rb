6bb7cb66fb7a6ab7a2b1425ce579d1210f78e2c2
Graham Turner <turnertgraham@gmail.com>
2016-07-07 16:19:40 -0400

    build out connect4 classes

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,1 +1,53 @@
-# Your code here!
+
+
+#loop until gameover?
+  #get move
+  #display board
+#end
+
+# class Game
+
+  # intialize(num_players)
+    #determine_players(num_players)
+    #@board = Board.new
+  #end
+
+  #determine_players
+   #generates number of players playing
+  #end
+
+  #def play
+  #end
+
+  #gameover
+  #end
+
+  #get_move
+  #end
+
+  #display_board
+  #end
+
+# class Board
+  #def intialize
+    @board = {1 => Array.new(7),
+              2 => Array.new(7),
+              3 => Array.new(7),
+              4 => Array.new(7),
+              5 => Array.new(7),
+              6 => Array.new(7)}
+  #end
+
+  #populate_board
+  #end
+
+# class Player
+  #get_input
+  #end
+
+# class Computer
+  #get_input
+  #end
+
+# class Human
+# class Display
