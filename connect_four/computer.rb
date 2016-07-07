b9673535be3168cd77d393d2a0a4961db6a4e03a
David Watts <davidjeremywatts@gmail.com>
2016-07-07 16:39:58 -0700

    add some basic AI

--- connect_four/computer.rb
+++ connect_four/computer.rb
@@ -8,17 +8,20 @@ class Computer
 
   def get_input(board)
     loop do
-      input = ask_for_input
+      input = ask_for_input(board.grid)
       if valid_input?(input, board)
         return input
         break
       end
-      
+
     end
   end
 
-  def ask_for_input
-    rand(7) + 1
+  def ask_for_input(grid)
+    (0..6).to_a.reverse.each do |x|
+      return x+1 if (grid[x].length == 0 || grid[x][-1] != "O") && grid[x].length !=6
+    end
+    return rand(7) + 1
   end
 
   def valid_input?(input, board)
