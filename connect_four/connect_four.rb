62416106d3a9a91909b19b0d8b7f0169b4c8a5f5
Adrian Mui <adrian.mui@gmail.com>
2016-07-07 13:41:27 -0400

    add pseodocode in connect_four.md

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,1 +1,27 @@
-# Your code here!
+#pseudocode
+
+Set up the game initially 
+    Create a game board
+    Create Players(2)
+Start the game loop
+    Render the board
+    Ask for and validate current player move
+    Update board 
+    Check for game over conditions
+      if the game should end 
+        Display the proper victory/loss message
+        break looping
+      else
+          Switch to the next player and keep looping
+
+Classes:
+    1. Game (itself)
+    2. Player 
+        2.5.  Human < Player
+        2.6.  Computer < Player
+    3. Board
+
+
+
+
+
