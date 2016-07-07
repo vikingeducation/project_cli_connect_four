fd749a4d147cc0331ca8d16a6c517c0ad417fd90
David Watts <davidjeremywatts@gmail.com>
2016-07-07 10:57:50 -0700

    start connect_four methods, create class rb files

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,20 +1,47 @@
-class Board
-  #7 columns, 6 rows
 
-  #rows (maybe use #transpose)
-  #columns(each array is a column)
-  #
+class ConnectFour
+
+  def initialize
+    @player1 = Player.new("Player 1", "O")
+    @player2 = Player.new("Player 2", "X")
+    @game_board = Board.new
+    @current_player = @player1
+  end
+
+  def play
+    puts "Welcome to Connect Four!"
+
+    loop do
+      ## render Board  (Display)
+
+      ## get move from player   (Player method)
+
+      ## check game_over?  break    (Board method)
+
+      switch_player
+
+    end
+
+    ## render Board  (Display)
+
+    ## Game end message  (ConnectFour end_message method)
+
+  end
+
+  def switch_player
+    if @current_player == @player1
+      @current_player = @player2
+    else
+      @current_player = @player1
+    end
+  end
+
+  # def end_message
+  #   case (player1 wins, player2 wins, draw)
+  #     ""
+  #     ""
+  #     ""
+  # end
 
 
-class Player
-  #get_move
-  #valid_input? (check if array is full)
-  #ask_for_input (puts prompt)
-
-
-class Game
-  #play loop
-
-
-module Display
-  #render(board)
+end
