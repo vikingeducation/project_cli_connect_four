5cf10c81041900640e943d1b0925c3e8f8cfa609
Leo Ahnn <leoahnn@gmail.com>
2016-07-07 17:22:52 -0400

    finished diagonal methods

--- connect_four/board.rb
+++ connect_four/board.rb
@@ -31,17 +31,50 @@ class Board
   end
 
   def rising_diagonals
-    diagonals = []
+    rising_diagonals = []
     (0..2).each do |start|
-      i = start
-      while i < 6
-        diagonals << @board[0][i]
-        i += 1
+      col = 0
+      diagonals = []
+      (start..5).each do |row|
+        diagonals << @board[col][row]
+        col += 1
       end
+      rising_diagonals << diagonals
     end
+    (1..3).each do |start|
+      row = 0
+      diagonals = []
+      (start..6).each do |col|
+        diagonals << @board[col][row]
+        row += 1
+      end
+      rising_diagonals << diagonals
+    end
+    rising_diagonals
   end
 
   def falling_diagonals
+    falling_diagonals = []
+    (3..5).each do |start|
+      col = 0
+      diagonals = []
+      start.downto(0) do |row|
+        diagonals << @board[col][row]
+        col += 1
+      end
+      falling_diagonals << diagonals
+    end
+    (1..3).each do |start|
+      row = 0
+      diagonals = []
+      start.downto(0) do |col|
+        diagonals << @board[col][row]
+        row += 1
+      end
+      falling_diagonals << diagonals
+    end
+    falling_diagonals
+
   end
 
 end
