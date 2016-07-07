350a279e363103b0f2f0efe6555c81e6413b2927
Morgan Martin <morganm5201@gmail.com>
2016-07-07 15:02:15 -0400

    add piece adding functionality

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,3 +1,48 @@
+
+class Game
+
+  def initialize
+    welcome
+    play
+  end
+
+  def play
+    until win?
+      @player1.ask_move
+      @player2.ask_move
+      [@player1, @player2].each_with_index do |player, index| 
+        @board.add_piece(player, index)
+      end
+    end
+  end
+
+  def welcome
+    #message
+    @board = Board.new
+    ask_mode
+    set_players
+  end
+
+  def set_players
+    case @mode
+    when :human
+      #enter name
+      @player1 = Player.new(gets.chomp)
+      #enter name
+      @player = Player.new(gets.chomp)
+    when :computer
+      #enter name
+      @player1 = Player.new(gets.chomp)
+      #set computer
+      @player2 = Player.new(:computer)
+    end
+  end
+
+  def win?
+  end
+
+end
+
 class Board
 
   def initialize
@@ -21,55 +66,25 @@ class Board
     return_array
   end
 
-end
-
-class Game
-
-  def initialize
-    welcome
-    play
-  end
-
-  def play
-    until win?
-      @player1.ask_move
-      @player2.ask_move
+  # Handles the adding of pieces
+  def add_piece(col, type)
+    @state.reverse_each do |row|
+      insert_peg(col, row, type) unless peg_present(col, row)
     end
   end
 
-  def welcome
-    #message
-    ask_mode
-    set_players
-    set_board
+  # Inserts a type into a supplied row at col
+  def insert_peg(col, row, type)
+    row[col] = convert_type(type)
   end
 
-  def set_players
-    case @mode
-    when :human
-      #enter name
-      @player1 = Player.new(gets.chomp)
-      #enter name
-      @player = Player.new(gets.chomp)
-    when :computer
-      #enter name
-      @player1 = Player.new(gets.chomp)
-      #set computer
-      @player2 = Player.new(:computer)
-    end
+  # Converts player's index to type
+  def convert_type(type)
+    type == 0 ? '=' : '+'
   end
-
-  #create Board instance. set as ivar?
-  def set_board
-  end
-
-  def win?
-  end
-
-end
 
 class Player
-
+  attr_reader :move
   def initialize(name)
     case name
     when String
@@ -77,6 +92,12 @@ class Player
       @mode = :human
     when Symbol
       @mode = :computer
+    end
+  end
+
+  def ask_move
+    puts "Which column would you like to place your peg in?"
+    gets.chomp.to_i - 1
   end
 
 end
