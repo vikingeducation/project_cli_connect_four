8487866de64feba523437a43f27ac83421313497
cjvirtucio87 <cjv287@gmail.com>
2016-07-07 13:57:20 -0700

    Debugging.

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -17,21 +17,26 @@ class Game
   end
 
   def welcome
-    #message
+    puts "Welcome to Connect Four?"
     @board = Board.new
     ask_mode
     set_players
   end
 
+  def ask_mode
+    puts "How many players? [one, two]"
+    @mode = gets.chomp == 'one' ? :computer : :human
+  end
+
   def set_players
     case @mode
     when :human
-      #enter name
+      puts "Enter Player 1's name."
       @player1 = Player.new(gets.chomp)
-      #enter name
+      puts "Enter Player 2's name."
       @player = Player.new(gets.chomp)
     when :computer
-      #enter name
+      puts "Enter Player 1's name."
       @player1 = Player.new(gets.chomp)
       #set computer
       @player2 = Player.new(:computer)
@@ -39,6 +44,9 @@ class Game
   end
 
   def win?
+    @board.any_winning_rows?
+    @board.any_winning_cols?
+    @board.any_winning_diagonals?
   end
 
 end
@@ -47,21 +55,17 @@ class Board
 
   def initialize
     @state = [
-      [o,o,o,o],
-      [o,o,o,o],
-      [o,o,o,o],
-      [o,o,o,o]
+      ["o","o","o","o"],
+      ["o","o","o","o"],
+      ["o","o","o","o"],
+      ["o","o","o","o"]
     ]
   end
 
-  def get_row(player_input)
-    @state[player_input-1]    
-  end
-
-  def get_col(player_input)
+  def get_col(col)
     return_array = []
     @state.each do |row|
-      return_array.push(row[player_input-1])
+      return_array.push(row[col])
     end
     return_array
   end
@@ -69,7 +73,10 @@ class Board
   # Handles the adding of pieces
   def add_piece(col, type)
     @state.reverse_each do |row|
-      insert_peg(col, row, type) unless peg_present(col, row)
+      unless peg_present(col, row)
+        insert_peg(col, row, type)
+        break
+      end
     end
   end
 
@@ -82,6 +89,44 @@ class Board
   def convert_type(type)
     type == 0 ? '=' : '+'
   end
+
+  #Check if peg already present.
+  def peg_present(col,row)
+    row[col] == '+' || row[col] == '='
+  end
+
+  def winning_combination?(combination)
+    if combination.all? { |peg| peg == "+" } || combination.all? { |peg| peg == "=" }
+      return true
+    end
+  end
+
+  #Winning conditionals
+  def any_winning_rows?
+    @state.each do |row|
+      winning_combination? row
+    end
+  end
+
+  def any_winning_cols?
+    (0..@state.first.length).each do |i|
+      winning_combination? get_col(i)
+    end
+  end
+
+  def any_winning_diagonals?
+    diagonal_1 = [@state[0][0],
+                  @state[1][1],
+                  @state[2][2],
+                  @state[3][3]]
+    diagonal_2 = [@state[0][3],
+                  @state[1][2],
+                  @state[2][1],
+                  @state[3][0]]
+    winning_combination?(diagonal_1) || winning_combination?(diagonal_2)
+  end
+
+end
 
 class Player
   attr_reader :move
@@ -96,8 +141,20 @@ class Player
   end
 
   def ask_move
-    puts "Which column would you like to place your peg in?"
-    gets.chomp.to_i - 1
+    case @move
+    when :human
+      puts "Which column would you like to place your peg in?"
+      gets.chomp.to_i - 1
+    when :computer
+      (0..3).sample
+    end
   end
 
 end
+
+
+def test
+  Game.new
+end
+
+test
