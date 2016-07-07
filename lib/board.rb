5a29b4cb1144cf958b02b919ad0e2e91b68dd51a
Conor Fischer <conor.fischer@gmail.com>
2016-07-06 21:20:10 -0500

    Add add peice to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,9 @@
 class Board
-	attr_reader :board_array
+	attr_reader :board_array, :last_move
 
 	def initialize( board_array = nil)
 		@board_array = board_array || Array.new(6) { Array.new(7) }
+		@last_move = [nil,nil]
 	end
 
 	def render
@@ -14,4 +15,26 @@ class Board
 			puts
 		end
 	end
+
+	def add_piece(column, peice)
+		if with_valid_column?(column)
+			@board_array.each_with_index do |item,row|
+				#puts "row = #{row} & column = #{column} item =  #{item}"
+				if item[column].nil?
+					@board_array.reverse[row][column] = peice
+					@last_move = [row,column]
+					return true
+					break
+				end
+			end
+			return false
+		else
+			return false
+		end
+	end
+
+	def with_valid_column?(column)
+		(0..6).include?(column) ? true : puts("Piece Out of Bounds")
+	end
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -22,7 +22,7 @@ describe Board do
 										[nil,nil,nil,nil,nil,nil,nil],
 										[nil,nil,nil,nil,"X",nil,nil],
 										[nil,nil,nil,"O","X",nil,nil],
-										["O","O","X","O","X",nil,nil]])
+										["O","O","X","O","X",nil,nil]] )
 			expect{progress_board.render}.to output("\n-------\n-------\n-------\n----X--\n---OX--\nOOXOX--\n").to_stdout
 		end
 
@@ -32,8 +32,34 @@ describe Board do
 									[nil,nil,nil,nil,"X",nil,nil],
 									[nil,nil,nil,nil,"X",nil,nil],
 									[nil,nil,nil,"O","X",nil,nil],
-									["O","O","X","O","X",nil,nil]])
+									["O","O","X","O","X",nil,nil]] )
 			expect{won_board.render}.to output("\n-------\n-------\n----X--\n----X--\n---OX--\nOOXOX--\n").to_stdout
+		end
+	end
+
+	describe "#add_piece" do
+		it "add valid move to board" do
+			subject.add_piece(4,"X")
+			expect(subject.board_array).to eq( [[nil,nil,nil,nil,nil,nil,nil],
+												[nil,nil,nil,nil,nil,nil,nil],
+												[nil,nil,nil,nil,nil,nil,nil],
+												[nil,nil,nil,nil,nil,nil,nil],
+												[nil,nil,nil,nil,nil,nil,nil],
+												[nil,nil,nil,nil,"X",nil,nil]] )
+		end
+
+		it "returns flase if column is full" do
+			full_column_board = Board.new( [[nil,nil,nil,nil,"O",nil,nil],
+											[nil,nil,nil,nil,"X",nil,nil],
+											[nil,nil,nil,nil,"O",nil,nil],
+											[nil,nil,nil,nil,"X",nil,nil],
+											[nil,nil,nil,nil,"O",nil,nil],
+											[nil,nil,nil,nil,"X",nil,nil]] )
+			expect(full_column_board.add_piece(4,"X")).to be false
+		end
+
+		it "returns flase if column is not on board" do
+			expect(subject.add_piece(10,"X")).to be false
 		end
 	end
 end
