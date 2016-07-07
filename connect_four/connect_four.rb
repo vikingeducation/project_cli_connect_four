ce2f3c4f82a0fd215e8d0a2c012d17b5fd0d1c0c
schoetlr <srlrs20020@gmail.com>
2016-07-07 13:32:15 -0400

    planning

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,1 +1,20 @@
-# Your code here!
+class Board
+  #7 columns, 6 rows
+
+  #rows (maybe use #transpose)
+  #columns(each array is a column)
+  #
+
+
+class Player
+  #get_move
+  #valid_input? (check if array is full)
+  #ask_for_input (puts prompt)
+
+
+class Game
+  #play loop
+
+
+module Display
+  #render(board)
