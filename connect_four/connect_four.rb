592e8a59047b0b34dc60ecc1f45fbb4639c4223f
David Jiang <davidjgrey@gmail>
2016-07-07 11:22:09 -0700

    laying out classes and methods

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,1 +1,12 @@
 # Your code here!
+# Classes: Player, Human, Computer, Game, Board, Column
+# Board: 7 across, 6 up
+
+# Board should be an array of arrays of Columns
+
+# Player: initialize, get_move
+# Human: valid_move? prompt
+# Computer: print_choice
+# Game: Initialize, play, win?, full?
+# Board: render
+# Column: initialize, show
