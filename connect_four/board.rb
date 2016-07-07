40acd97b9a23a0fbc518e17b0a96e993950d590c
David Watts <davidjeremywatts@gmail.com>
2016-07-07 11:47:57 -0700

    board diagonals

--- connect_four/board.rb
+++ connect_four/board.rb
@@ -10,11 +10,31 @@ class Board
     grid.chunk { |piece| piece }.map{|a,b| b}.any? { |arr| arr.count >= 4 }
   end
 
-  def horizontal_win?(grid)   
+  def horizontal_win?(grid)
     vertical_win?(grid.transpose)
   end
 
+  def diagonal_win?(grid)
+    vertical_win?(diagonalize(grid))
+  end
 
+  def diagonalize(grid)
+    copy = fill_copy(grid)
+    arr = []
+
+
+
+  end
+
+  def fill_copy(grid)
+    copy = grid
+    copy.length.times do |x|
+      until copy[x].length == 7
+        x << "x"
+      end
+    end
+    copy
+  end
 
 
 end
