549f244d689c25398373f0e305732f8df88af903
cjvirtucio87 <cjv287@gmail.com>
2016-07-08 11:31:27 -0700

    Added Board tests up to #add_piece.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,9 +2,9 @@ require 'board'
 
 describe Board do
   let(:board) { Board.new }
+  let(:board_grid) { board.instance_variable_get( :@board ) }     
 
   describe "#initialize" do
-    let(:board_grid) { board.instance_variable_get( :@board ) }
 
     it "creates an instance of board" do
       expect(board).to be_an_instance_of(Board)
@@ -16,7 +16,28 @@ describe Board do
     end
 
     it "when board is initialized, columns are empty" do
-
+      expect(board_grid.all?(&:empty?)).to eq(true)
     end
+  end
+
+  describe "#add_piece" do
+    before(:each) do
+      board.add_piece('r',0)
+    end
+
+
+    it "adds the correct color piece in the correct spot" do
+      expect(board_grid.first[0]).to eq('r')
+    end
+
+    it "only adds to the correct spot" do
+      board_size = board_grid.reduce(0) do |piece_total,col|
+        piece_total + col.length
+      end
+      expect(board_size).to eq(1)
+    end
+
+    #only adds to the correct spot
+    #col must be available
   end
 end
