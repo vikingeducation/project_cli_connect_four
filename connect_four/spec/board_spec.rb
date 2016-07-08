2f8d3cee9f799fdca91e332c84a65476b97fd2fa
Adrian Mui <adrian.mui@gmail.com>
2016-07-08 19:39:43 -0400

    board_spec.rb test finish

--- connect_four/spec/board_spec.rb
+++ connect_four/spec/board_spec.rb
@@ -27,18 +27,26 @@ describe Board do
   end
 
   describe '#column_full?' do
-    it 'checks if column is full' do
+    it 'returns true if column is full' do
       (abc.game_board[1].length).times { abc.add_piece(1, :x) }
       expect(abc.column_full?(1)).to be true
+    end
+
+    it 'returns false if column is not full' do
+      expect(abc.column_full?(1)).to be false
     end
   end
 
   describe '#full?' do
-    it 'checks if board is completely full' do
+    it 'returns true if board is completely full' do
       d.game_board.each_key do |key|
         (d.game_board[key].length).times { d.add_piece(key, :x) }
       end
       expect(d.full?).to be true
+    end
+
+     it 'returns false if board is not completely full' do
+      expect(d.full?).to be false
     end
   end
 
@@ -115,16 +123,27 @@ describe Board do
       end
 
       expect(d.to_the_left_diagonals_down[0]).to match_array([:clear,:o,:x,:clear,:o,:x])
-      #expect(d.to_the_left_diagonals_down[2]).to match_array([:x,:clear,:o,:x])
+      expect(d.to_the_left_diagonals_down[2]).to match_array([:x,:clear,:o,:clear]) #..
     end
   end
 
   describe '#verticals' do
     it 'returns all vertical arrays' do
-      d.game_board.each_key do |key|
-        (d.game_board[key].length).times { d.add_piece(key, :x) }
-      end
+      (d.game_board[1].length).times { d.add_piece(1, :x) }
       expect(d.verticals[0]).to match_array([:x, :x, :x, :x, :x, :x])
+      expect(d.verticals[1]).to match_array([:clear, :clear, :clear, :clear, :clear, :clear])
     end
   end
+
+  describe '#horizontals' do
+    it 'returns all the horizontal arrays' do
+      d.game_board.each_key do |key| 
+        d.add_piece(key, :x) 
+        d.add_piece(key, :o)
+      end
+
+      expect(d.horizontals[-1]).to match_array([:x, :x, :x, :x, :x, :x, :x])
+      expect(d.horizontals[-2]).to match_array([:o, :o, :o, :o, :o, :o, :o])
+    end
+   end
 end
