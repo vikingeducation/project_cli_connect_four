c7fa004ccdf367eb9c8219d6c773f9789de1725f
Morgan Martin <morganm5201@gmail.com>
2016-07-08 11:57:08 -0400

    Fix win checking

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,64 +1,263 @@
-require 'pry'
 class Game
-
   def initialize
-    welcome
+    @view = View.new
+    @board = Board.new
+    set_up_game
     play
   end
 
-  def play
-    until win?
-      @player1.ask_move
-      @player2.ask_move
-      [@player1, @player2].each_with_index do |player, index| 
-        @board.add_piece(player.move, index)
-      end
-      @board.render
-    end
-    puts "Congratulations! Game over."
-  end
-
-  def welcome
-    puts "Welcome to Connect Four?"
-    @board = Board.new
-    ask_mode
+  def set_up_game
+    set_view_board
+    @view.welcome_message
+    @mode = @view.ask_for_mode
     set_players
   end
 
-  def ask_mode
-    puts "How many players? [one, two]"
-    @mode = gets.chomp == 'one' ? :computer : :human
+  def set_view_board
+    @view.board = @board
+  end
+
+  def play
+    until @board.win?
+      @player1.make_move
+      @player2.make_move
+      @board.render
+    end
+    @board.render
+    set_winner
+    @view.end_message(@winner)
+  end
+
+  def set_winner
+    @winner = find_player_by_symbol(@board.winner_symbol)
+    @board.winner = @winner
+  end
+
+  def find_player_by_symbol(symbol)
+    @player1.symbol == symbol ? @player1 : @player2
   end
 
   def set_players
+    @player1 = Human.new('=' , @view, @board)
+    puts "Mode is #{@mode}"
     case @mode
-    when :human
-      puts "Enter Player 1's name."
-      @player1 = Player.new(gets.chomp)
-      puts "Enter Player 2's name."
-      @player2 = Player.new(gets.chomp)
     when :computer
-      puts "Enter Player 1's name."
-      @player1 = Player.new(gets.chomp)
-      #set computer
-      @player2 = Player.new(:computer)
+      @player2 = Computer.new('+', @view, @board)
+    when :human
+      @player2 = Human.new('+', @view, @board)
     end
   end
+end
 
-  def win?
-    #@board.any_winning_rows? ||
-    #@board.any_winning_cols? ||
-    #@board.any_winning_diagonals?
-    @board.check_for_wins
+class Player
+  attr_reader :symbol, :name
+  def initialize(symbol, view, board)
+    @symbol = symbol
+    @view = view
+    @board = board
+    set_name
+  end
+end
+
+class Human < Player
+
+  def initialize(symbol, view, board)
+    super(symbol, view, board)
   end
 
+  def set_name
+    @name = @view.ask_for_name
+  end
+
+  def make_move
+    move = get_appropriate_move
+    @board.add_disc(move, @symbol)
+  end
+
+  def get_appropriate_move
+    move = nil
+    until @board.possible_col?(move)
+      move = @view.ask_for_col(@name) - 1
+    end
+    move
+  end
+end
+
+class Computer < Player
+  def initialize(symbol, view, board)
+    super(symbol, view, board)
+  end
+
+  def set_name
+    @name = "Computer"
+  end
+
+  def make_move
+    @board.add_disc(rand(7), @symbol)
+  end
+end
+
+class View
+  attr_accessor :board
+
+  def end_message(winner)
+    if winner.is_a?(Computer)
+      write_out("The computer wins...")
+    else
+      write_out("Congratulations #{@board.winner.name}, you win!")
+    end
+    nil
+  end
+
+  def ask_for_col(entity = nil)
+    write_out("Which column would you like to place your disc in, #{entity}?")
+    get_input.to_i
+  end
+
+  def welcome_message
+    write_out("Welcome to Connect Four!")
+  end
+
+  def ask_for_name(entity = nil)
+    if entity
+      write_out("And what is your name, #{entity}?")
+    else
+      write_out("And what is your name?")
+    end
+    get_input.to_s
+  end
+
+  def write_out(output)
+    puts output
+  end
+
+  def get_input
+    gets.chomp
+  end
+
+  def ask_for_mode
+    modes = %w(one two)
+    input = nil
+    until modes.include?(input)
+      write_out("How many players? [one, two]")
+      input = get_input
+    end
+    input.to_sym == :one ? :computer : :human
+  end
 end
 
 
 class Board
+  attr_reader :winner_symbol
+  attr_accessor :winner
+  def initialize(state = new_game_board)
+    @state = state
+  end
 
-  def initialize
-    @state = [
+  def win?
+    any_horizontal_wins? ||
+    any_vertical_wins?   ||
+    any_diagonal_wins?
+  end
+
+  def any_horizontal_wins?
+    @state.each { |row| return true if row_contains_horizontal_wins?(row) }
+    false
+  end
+
+  def any_vertical_wins?
+    @state.transpose.each do |row|
+      return true if row_contains_horizontal_wins?(row)
+    end
+    false
+  end
+
+  def any_diagonal_wins?
+    @state.each_with_index do |row, row_index|
+      row.each_with_index do |col, col_index|
+        combination = find_se_diag_by_row_col(row_index, col_index)
+        return true if winning_combination?(combination)
+      end
+    end
+    false
+  end
+
+  def find_se_diag_by_row_col(row, col)
+    positions = [ [row,col], [(row+1),(col+1)], [(row+2),(col+2)], [(row+3),(col+3)] ]
+    positions.map do |position|
+      row, col = position[0], position[1]
+      if possible_col?(col) && possible_row?(row)
+        @state[row][col]
+      else
+        nil
+      end
+    end
+  end
+
+  def row_contains_horizontal_wins?(row)
+    4.times do |iter|
+      next unless possible_row?(iter+3)
+      combination = row[iter..(iter+3)]
+      return true if winning_combination?(combination)
+    end
+    false
+  end
+
+  def winning_combination?(combination)
+    if combination.all? { |disc| disc == "=" }
+      @winner_symbol = "="
+      true
+    elsif combination.all? { |disc| disc == "+" }
+      @winner_symbol = "+"
+      true
+    else
+      false
+    end
+  end
+
+  def possible_row?(row_num)
+    row_num > -1 && row_num < 6
+  end
+
+  def possible_col?(col_num)
+    return false if col_num.nil?
+    col_num > -1 && col_num < 7
+  end
+
+  def render
+    @state.each do |row|
+      p row
+    end
+    puts
+  end
+
+  # Handles the adding of discs
+  def add_disc(col, type)
+    @state.reverse_each do |row|
+      unless disc_present(col, row)
+        insert_disc(col, row, type)
+        break
+      end
+    end
+  end
+
+  # Inserts a type into a supplied row at col
+  def insert_disc(col, row, type)
+    row[col] = convert_type(type)
+  end
+
+  # Converts from := or :+ symbols to matching strings
+  def convert_type(disc)
+    disc.to_s
+  end
+
+  #Check if disc already present.
+  def disc_present(col,row)
+    row[col] == '+' || row[col] == '='
+  end
+
+  def new_game_board
+    [
       ["o","o","o","o","o","o"],
       ["o","o","o","o","o","o"],
       ["o","o","o","o","o","o"],
@@ -67,208 +266,15 @@ class Board
       ["o","o","o","o","o","o"]
     ]
   end
-
-  def check_for_wins
-    0.upto(5) do |row|
-      0.upto(6) do |col|
-        DirectionGenerator.generate_directions(row,col,[6,5]).each do |combo|
-          combo.map{ |item| @state[item[0]][item[1]] }
-          return true if winning_combination?(combo)
-        end
-      end
-    end
-    false
-  end
-
-  def render
-    @state.each do |row|
-      p row
-    end
-  end
-
-  def get_col(col)
-    return_array = []
-    @state.each do |row|
-      return_array.push(row[col])
-    end
-    return_array
-  end
-
-  # Handles the adding of pieces
-  def add_piece(col, type)
-    @state.reverse_each do |row|
-      unless peg_present(col, row)
-        insert_peg(col, row, type)
-        break
-      end
-    end
-  end
-
-  # Inserts a type into a supplied row at col
-  def insert_peg(col, row, type)
-    row[col] = convert_type(type)
-  end
-
-  # Converts player's index to type
-  def convert_type(type)
-    type == 0 ? '=' : '+'
-  end
-
-  #Check if peg already present.
-  def peg_present(col,row)
-    row[col] == '+' || row[col] == '='
-  end
-
-  def winning_combination?(combination)
-    combination.all? { |peg| peg == "+" } || combination.all? { |peg| peg == "=" }
-  end
-
-  #Winning conditionals
-  def any_winning_rows?
-    # binding.pry
-    @state.each do |row|
-      return true if winning_combination? row
-    end
-    false
-  end
-
-  def any_winning_cols?
-    (0..@state.first.length).each do |i|
-      return true if winning_combination? get_col(i)
-    end
-    false
-  end
-
-  def any_winning_diagonals?
-    diagonal_1 = [@state[0][0],
-                  @state[1][1],
-                  @state[2][2],
-                  @state[3][3]]
-    diagonal_2 = [@state[0][3],
-                  @state[1][2],
-                  @state[2][1],
-                  @state[3][0]]
-    winning_combination?(diagonal_1) || winning_combination?(diagonal_2)
-  end
-
 end
 
-class Player
-  attr_reader :move
-  def initialize(name)
-    case name
-    when String
-      @name = name
-      @mode = :human
-    when Symbol
-      @mode = :computer
-    end
-  end
-
-  def ask_move
-    case @mode
-    when :human
-      puts "Which column would you like to place your peg in?"
-      @move = gets.chomp.to_i - 1
-    when :computer
-      @move = rand(4)
-    end
-  end
-
-end
-
-class DirectionGenerator
-  attr_reader :combinations
-
-  DIRECTIONS = [
-    :n,
-    :s,
-    :e,
-    :w,
-    :nw,
-    :ne,
-    :sw,
-    :se
+def test_game_board
+  [
+    ["o","o","o","o","o","o"],
+    ["o","o","o","o","o","o"],
+    ["=","o","o","o","o","o"],
+    ["=","o","o","o","o","o"],
+    ["=","o","o","o","o","o"],
+    ["=","o","o","o","o","o"]
   ]
-
-  class << self
-
-    def generate_directions(row, col, limit)
-      DIRECTIONS.map do |direction| 
-        dispatch(direction, row, col)
-      end.select do |row|
-        row.none? do |coord|
-          bad_coord?(coord, limit)
-        end
-      end
-    end
-
-    def bad_coord?(coord, limit)
-      coord[0] > limit[0] || coord[0] < 0 || coord[1] > limit[1] || coord[1] < 0
-    end
-
-    def dispatch(direction,row,col)
-      case direction
-      when :nw
-        generate_diagonal(row,col,:-,:-)
-      when :ne
-        generate_diagonal(row,col,:-,:+)
-      when :sw
-        generate_diagonal(row,col,:+,:-)
-      when :se
-        generate_diagonal(row,col,:+,:-)
-      when :n
-        generate_cardinal(row,col,:row,:-)
-      when :s
-        generate_cardinal(row,col,:row,:+)
-      when :e
-        generate_cardinal(row,col,:col,:+)
-      when :w
-        generate_cardinal(row,col,:col,:-)
-      end
-    end
-
-    def generate_cardinal(row,col,changing, op)
-      return_array = []
-      if changing == :row
-        if op == :+
-          (row..(row+3)).to_a.each do |row_num|
-            return_array << [row_num, col]
-          end
-        elsif op == :-
-          ((row-3)..row).to_a.each do |row_num|
-            return_array << [row_num, col]
-          end
-        end
-      elsif changing == :col
-        if op == :+
-          (col..(col+3)).to_a.each do |col_num|
-            return_array << [row, col_num]
-          end
-
-        elsif op == :-
-          ((col-3)..col).to_a.each do |col_num|
-            return_array << [row, col_num]
-          end
-        end
-      end
-      return_array
-    end
-
-    def generate_diagonal(row,col,op,op2)
-      return_array = [[row,col]]
-      (1..3).to_a.each do |term|
-        return_array << [(row.send(op,term)),(col.send(op2,term))]
-      end
-      return_array
-    end
-
-  end
 end
-
-
-def test
-  Game.new
-end
-
-test
