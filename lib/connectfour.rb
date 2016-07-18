fcf02d71f275bb5eff45150fc3cf4032f3c9a47a
Conor Fischer <conor.fischer@gmail.com>
2016-07-17 21:34:35 -0500

    Add play function to run game

--- lib/connectfour.rb
+++ lib/connectfour.rb
@@ -1,3 +1,35 @@
-class Connectfour
+require_relative "../lib/player.rb"
+require_relative "../lib/board.rb"
 
+class Connectfour
+	def initialize
+		@board = Board.new
+
+		@player_1 = Player.new("Player 1", "X", @board)
+		@player_2 = Player.new("Player 2", "O", @board)
+
+		@current_player = @player_1
+	end
+
+	def play
+		loop do
+			@board.render
+
+			@current_player.get_user_selection
+			if @current_player.check_win?
+				puts "Win!!!!"
+				break
+			end
+
+			swap_players
+		end
+	end
+
+	def swap_players
+		if @current_player == @player_1
+			@current_player = @player_2
+		else
+			@current_player = @player_1
+		end
+	end
 end
--- spec/connectfour_spec.rb
+++ spec/connectfour_spec.rb
@@ -4,4 +4,6 @@ describe Connectfour do
    	it "is a connect four" do
 		expect(subject).to be_a(Connectfour)
 	end
+
+	
 end
