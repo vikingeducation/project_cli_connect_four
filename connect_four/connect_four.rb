7bbda0d29063d77b0692d5e51e753d102ef69ae0
Visiona <darbis@gmail.com>
2016-10-22 00:00:44 +0100

    The main game is created with two players, two minor bugs remain to fix

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,127 +1,214 @@
 class ConnectFour
 
-def initialize
-  @board = Board.new
-  @player_one = Player.new()
-  @player_two = Player.new()
-end
+  def initialize
+    @board = Board.new
+    @player_one = HumanPlayer.new("PLAYER ONE", "@" ,@board)
+    @player_two = HumanPlayer.new("PLAYER TWO", "O" ,@board)
+    @current_player = @player_one
+  end
 
-# Play
-def play
-#   Welcome_display
-#   until board_is_full?
-#     render the board
-#     Ask player for movement
-#      HumanPlayer.new.get_position - ask player for movement
-#       is_there_winner?
-#         winner_looser_message
-#       draw_message if board_is_full?
-#     Switch players
-#     end
+  def play
+    welcome_display
+    loop do
+      @board.render
+      @current_player.get_guess
+      game_over
+      switch_players
+    end
+  end
 
-def welcome_display
-  puts "
-  ____                            _     _____
- / ___|___  _ __  _ __   ___  ___| |_  |  ___|__  _   _ _ __
-| |   / _ \\| '_ \\| '_ \\ / _ \\/ __| __| | |_ / _ \\| | | | '__|
-| |__| (_) | | | | | | |  __/ (__| |_  |  _| (_) | |_| | |
- \\____\\___/|_| |_|_| |_|\\___|\\___|\\__| |_|  \\___/ \\__,_|_|"
- puts
- puts
- puts "ABOUT THE GAME"
- puts "This game is similar to the classic tic-tac-toe game, the primary difference being that in this game players must connect four of their pegs in a straight line, either horizontally, vertically or diagonally."
- puts "The player takes turns with the other player or computer to drop pegs into slots within the grid. The first one to connect four slots in a line wins."
- puts
- puts
-end
+  def welcome_display
+    puts "
+    ____                            _     _____
+   / ___|___  _ __  _ __   ___  ___| |_  |  ___|__  _   _ _ __
+  | |   / _ \\| '_ \\| '_ \\ / _ \\/ __| __| | |_ / _ \\| | | | '__|
+  | |__| (_) | | | | | | |  __/ (__| |_  |  _| (_) | |_| | |
+   \\____\\___/|_| |_|_| |_|\\___|\\___|\\__| |_|  \\___/ \\__,_|_|"
+   puts
+   puts
+   puts "ABOUT THE GAME"
+   puts "This game is similar to the classic tic-tac-toe game, the primary difference being that in this game players must connect four of their pegs in a straight line, either horizontally, vertically or diagonally."
+   puts "The player takes turns with the other player or computer to drop pegs into slots within the grid. The first one to connect four slots in a line wins."
+   puts "The player drops his peg into chosen column. The pegs falls into next available position."
+   puts
+   puts
+  end
 
-def is_there_winner?
-  #checks if the game is won and who is the winner
-  #are diagonals, horizontals, verticals in four
-end
+  def is_there_winner?
+    if @board.winning_connected_four(@current_player.peg_symbol)
+      puts "Congratulations to #{@current_player.name}!!! You have connected four symbol pegs!"
+      true
+    else
+      false
+    end
+  end
 
-def is_there_draw?
+  def is_there_draw?
+    if @board.is_board_full?
+      puts "There is no winner this time!"
+      true
+    else
+      false
+    end
+  end
 
-end
+  def game_over
+    is_there_draw? || is_there_winner?
+  end
 
-def winner_looser_message
-  #display message for the winner and looser
-end
-# def winner_or_draw
-# => is_there_draw? || is_there_winner?
-#end
-
-
-def draw_message
-  #displays message when the board is ful and noone won
-end
-
-def switch_players
-  #changes turns of the players
-end
-
+  def switch_players
+    @current_player == @player_one ? @current_player = @player_two : @current_player = @player_one
+  end
 
 end
 
 class HumanPlayer
-  initilize pieces
-  initialize board
+  attr_accessor :peg_symbol
 
-  def get_position
-
-    is_valid_position?
-    is_valid_input
-
+  def initialize(name, peg_symbol ,board)
+    raise "Peg has to be a string" unless peg_symbol.is_a? (String)
+    @name = name
+    @peg_symbol = peg_symbol
+    @board = board
   end
 
-  def is_valid_position?
-    #can player move his piece there?
+  def get_guess
+    loop do 
+      guess = ask_for_guess
+      if @board.is_peg_location_available?(guess)
+        if @board.add_pegs(guess, peg_symbol)
+          break
+        end
+      end
+    end
   end
 
-  def is_valid_input?
-    #correctly input
+  def ask_for_guess
+    loop do
+      puts
+      puts
+      puts "#{@name}, Please type a number of the column you wish to drop your peg in #{@peg_symbol}"
+      guess = gets.strip.to_i
+      if is_valid_input?(guess)
+        break if @board.is_peg_location_available?(guess)
+      end
+      puts "Please provide correct number of the column!"
+    end
+  end
+
+  # def is_space_in_column?(guess)
+  #   @board.any? {|row| row[guess].nil? }
+  # end
+
+  def is_valid_input?(guess)
+    "1234567".include? guess.to_s
   end
 
 end
 
 class Board
 
-def initialize
-  @board = Array.new(7){Array.new(6)}
-def render
-  #displays the current board
+  def initialize
+    @board = Array.new(6){Array.new(7)}
+  end
+
+  def render
+    puts
+    puts "DBG: @board = #{@board.inspect}"
+    @board.size.times do |row|
+      print " 1  2  3  4  5  6  7 \n" if row == 0
+      @board[row].size.times {|col| print "|#{@board[row][col]}"}
+      puts
+    end
+      print "_____________________"
+  end
+
+  def is_board_full?
+    !@board.any? {|row| row.include? nil} # if true the board is not full
+  end
+
+  def add_pegs(guess, peg_symbol)
+    (5).downto(0) do |row|
+      if @board[row][guess-1].nil?
+        @board[row][guess-1] = peg_symbol
+        break
+      end
+    end
+  end
+
+  def is_peg_location_available?(guess)
+    if @board[0][guess].nil?
+      true
+    else
+      puts "Please choose different column, this one is full!"
+    end
+  end
+
+  def board_of_rows_strings
+    @board.map { |row| row = row.join(",") }
+  end
+
+  def is_it_winning_horizontal?(peg_symbol)
+    four = four_pegs_connected(peg_symbol)
+    board_of_rows_strings.any? {|row| row.include? four}
+  end
+
+  def is_it_winning_diagonal?(peg_symbol)
+    north_east_diagonals.any? {|row| row.include? four_pegs_connected(peg_symbol)} ||
+    north_west_diagonals.any? {|row| row.include? four_pegs_connected(peg_symbol)}
+  end
+
+  def is_it_winning_vertical?(peg_symbol)
+    board_of_columns_strings.any? {|row| row.include? four_pegs_connected(peg_symbol)}
+  end
+
+  def four_pegs_connected(peg_symbol)
+    "#{peg_symbol},#{peg_symbol},#{peg_symbol},#{peg_symbol}"
+  end
+
+  def winning_connected_four(peg_symbol)
+    is_it_winning_horizontal?(peg_symbol) ||
+    is_it_winning_diagonal?(peg_symbol) ||
+    is_it_winning_vertical?(peg_symbol)
+  end
+
+  def board_of_columns_strings
+    board_vertical = []
+    6.times do |column|
+      new_row = []
+      @board.each {|row| new_row << row[column]}
+      board_vertical << new_row
+    end
+    board_vertical.map { |row| row = row.join(",") }
+  end
+
+  def north_east_diagonals
+    ne_diagonals = Array.new(6){Array.new}
+      0.upto(5) do |diag|
+        ne_diagonals[0] << @board[0+diag][diag]
+        ne_diagonals[1] << @board[1+diag][diag] if 1 + diag < 6
+        ne_diagonals[2] << @board[2+diag][diag] if 2 + diag < 6
+        ne_diagonals[3] << @board[diag][1+diag]
+        ne_diagonals[4] << @board[diag][2+diag]
+        ne_diagonals[5] << @board[diag][3+diag]
+      end
+    ne_diagonals.map { |row| row = row.join(",") }
+  end
+
+  def north_west_diagonals
+    nw_diagonals = Array.new(6){Array.new}
+    0.upto(5) do |diag|
+      nw_diagonals[0] << @board[diag][4 + diag]
+      nw_diagonals[1] << @board[diag][5 + diag] if 6 + diag < 7
+      nw_diagonals[2] << @board[diag][6 + diag] if 6 + diag < 7
+      nw_diagonals[3] << @board[4 + diag][diag] if 4 + diag < 6
+      nw_diagonals[4] << @board[5 + diag][diag] if 5 + diag < 6
+      nw_diagonals[5] << @board[6 + diag][diag] if 6 + diag < 6
+    end
+    nw_diagonals.map { |row| row = row.join(",") }
+  end
+
 end
 
-def is_board_full?
-  #are there any free spaces left
-end
-
-def add_pegs
-
-end
-
-def is_peg_location_available?
-
-end
-
-def is_peg_location_on_board
-
-end
-
-def is_it_winning_combination
-
-end
-
-def is_it_winning_diagonal?
-
-end
-
-def is_it_winning_vertical?
-
-end
-
-def is_it_winning_horizontal?
-
-end
-
-end
+s = ConnectFour.new
+s.play
