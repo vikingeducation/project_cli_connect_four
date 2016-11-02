9915f7da63e400fc045b37b6133c8fa640650993
Alexa <alexa.anderson@outlook.com>
2016-11-02 13:41:29 -0500

    Add combo checkers

--- connect_four/board.rb
+++ connect_four/board.rb
@@ -8,13 +8,12 @@ class Board
     valid_drop?(col)
     row = get_row(col)
     position = [col, row]
-    position.winning_combos
     player == red ? @redsPositions << position : @bluesPositions << position
+    winnging_combos(position)
   end
 
   def get_row(col)
     row = col.length - 1
-    row
   end
 
   def not_full?(col)
@@ -30,34 +29,59 @@ class Board
   end
 
   def winning_combos(position)
-    vertical_win?(position)
+    combos = {}
+    combos[:vert] = vertical_combos(position)
+    combos[:hori] = horizontal_combos(position)
   end
 
-  def vertical_win?(position)
+  def vertical_combos(position)
+    [
+      [position,
+      [position[0], (position[1] - 1)],
+      [position[0], (position[1] - 2)],
+      [position[0], (position[1] - 3)]]
+    ]
+  end
 
+  def horizontal_combos(position)
+    # 4 arrays of 4 vectors each
+    # 3,4
+    hori_combos =[]
+    [(-3..0), (-2..1), (-1..2), (0..3)].each do |r| 
+      combos =[]
+      r.each do |num|
+        combos.push([(position[0] + num), position[1]])
+      end
+      hori_combos.push(combos)
+    end
+    hori_combos
+  end
+
+  def four_in_a_row?()
+    # check 
   end
 end
 
 
-2,1 -- 3,2 4,3 5,4
-       1,2 0,3 -,4
+# 2,1 -- 3,2 4,3 5,4
+#        1,2 0,3 -,4
 
-4,4 --increase 1,1 or -1,-1
-      3,5 2,6  1,-1   -1, 1
-      5,3 6,2
+# 4,4 --increase 1,1 or -1,-1
+#       3,5 2,6  1,-1   -1, 1
+#       5,3 6,2
 
-      [1,2]
+#       [1,2]
 
-# position class
-# any one middle position has max 16 wins
-# - has properties: color or nil, coordinates
+# # position class
+# # any one middle position has max 16 wins
+# # - has properties: color or nil, coordinates
 
-# When player drop piece generate a hash of winning combos or array of arrays
-# player only gives x,
-winningCombos = { :vert => [], :hori => [[]], :diag => [[]] }
+# # When player drop piece generate a hash of winning combos or array of arrays
+# # player only gives x,
+# winningCombos = { :vert => [], :hori => [[]], :diag => [[]] }
 
-# VECTOR
-(5, 4)
-# vertical win at [0, [-4]]
-# horizontal win at [[-4..4], 0]
-# diagonal [[4..-4], [4..-4](same number)] and [[-4..4], [4..-4] (opposite numbers)]
+# # VECTOR
+# (5, 4)
+# # vertical win at [0, [-4]]
+# # horizontal win at [[-4..4], 0]
+# # diagonal [[4..-4], [4..-4](same number)] and [[-4..4], [4..-4] (opposite numbers)]
