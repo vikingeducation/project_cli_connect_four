7706fff9fbc806cf5c05323dd3a2e6738fca100e
Pramod Jacob <psjacob68@gmail.com>
2016-11-03 10:34:46 -0400

    completed board.rb

--- connect_four/board.rb
+++ connect_four/board.rb
@@ -1,97 +1,160 @@
 class Board
 
   # board = [
-  #    0  1  2  3  4  5  6
-  #   [r, b, r, b, b, b, b], # board[0]
-  #   [0, 0, 0, 0, 0, b, 0], # board[1]
-  #   [0, 0, 0, 0, 0, b, 0], # board[2]
-  #   [0, 0, 0, 0, 0, b, 0], # board[3]
-  #   [0, 0, 0, 0, 0, 0, 0], # board[4]
-  #   [0, 0, 0, 0, 0, 0, 0]  # board[5]
-  # ]
+  #           0  1  2  3  4  5  6
+  #          [r, b, r, b, b, b, b], # board[0]
+  #          [0, 0, 0, 0, 0, b, 0], # board[1]
+  #          [0, 0, 0, 0, 0, b, 0], # board[2]
+  #          [0, 0, 0, 0, 0, b, 0], # board[3]
+  #          [0, 0, 0, 0, 0, 0, 0], # board[4]
+  #          [0, 0, 0, 0, 0, 0, 0]  # board[5]
+  #         ]
 
-  attr_accessor :board # temp
+  #### TEMPORARY
+  attr_accessor :board 
+  #### TEMPORARY
 
   def initialize
-    @board = # 0  1  2  3  4  5  6
-              [
-              [0, 0, 0, 0, 0, 0, 0], # 0
-              [0, 0, 0, 0, 0, 0, 0], # 1
-              [0, 0, 0, 0, 0, 0, 0], # 2
-              [0, 0, 0, 0, 0, 0, 0], # 3
-              [0, 0, 0, 0, 0, 0, 0], # 4
-              [0, 0, 0, 0, 0, 0, 0]  # 5
-              ]
+    @board = Array.new(6) { |row| [0, 0, 0, 0, 0, 0, 0] }
   end
 
-  # update board array
-  def update(input, color)
-    6.times do |i|
-      if @board[i][input - 1] == 0
-        @board[i][input - 1] = color
-        return
+  def update(col_num, piece) 
+    col_num -= 1
+    @board.length.times do |row_num|
+      if @board[row_num][col_num] == 0
+        @board[row_num][col_num] = piece 
+        break
       end
     end
-    self
+    self 
   end
 
-  # check that column is not full
-  def valid_move?(input)
-    @board[5][input - 1] == 0
+  def valid_move?(col_num)
+    last_row = 5
+    col_num -= 1
+    @board[last_row][col_num] == 0
   end
 
-  def horizontal_win?(color)
-    # each row that has 4 or more values
+  def four_in_a_row?(four_piece_array)
+    first_piece = four_piece_array[0]
+    four_piece_array.all? { |piece| piece == first_piece }
+  end
 
-    @board.each_with_index do |row_arr, row_num|
-
-      start_point = 0
-      end_point = 3
-
-      (start_point..end_point).each do |col_num|
-        return true if row_arr[col_num..col_num + 3].all? { |piece| piece == color }
+  def horizontal_win?
+    @board.each do |row|
+      start_point, end_point = 0, 3
+      (start_point..end_point).each do |offset|
+        four_piece_array = row[start_point+offset..end_point+offset]
+        next if four_piece_array.include?(0)
+        return true if four_in_a_row?(four_piece_array)
       end
     end
-
-    false
-
-    #
-      # find a starting coordinate [0, 1, 2, 3] == player
-        # check the final coordinate: [index + 3] == player
-          # check the 3rd coordinate: [index + 2]
-            # check the 2nd coord: [index + 1]
-              # return true => win condition
-  end
-
-  def vertical_win?(color)
     false
   end
 
-  def diagonal_win?(color)
+  def vertical_win?
+    @board = @board.transpose
+    result = horizontal_win?
+    @board = @board.transpose
+    result
+  end
+
+  def diagonal_win?
+    rows_to_check = @board[0..2]
+    rows_to_check.each_with_index do |row, row_num| # row_num = 0, 1, 2
+      start_point, end_point = 0, 3
+      (start_point..end_point).each do |col_num| # col_num = 0, 1, 2, 3
+        offset = 0
+        four_piece_array = []
+        while offset < 4
+          four_piece_array << @board[row_num+offset][col_num+offset]
+          offset += 1
+        end
+        next if four_piece_array.include?(0)    
+        return true if four_in_a_row?(four_piece_array) 
+      end
+    end
     false
   end
 
-
-
-  # vertical:
-    # use .transpose
-
-  # diagonal:
-    # starting points => x: [0..3] y: [0..2]
-    # coordinate system
-      # check if a coordinate == player/color: [0][0]
-        # find the last (4th value) [index+3][index+3] == player
-          # find 3rd
-            # find 2nd
-              # return true
-    # reverse each array => repeat
-
-  # instance of board: keeps track of moves
-  # return what the board is
-
-  # if board is full, returns true
   def full?
     !@board.flatten.include?(0)
   end
 
 end
+
+# t = Board.new
+
+# t.board = [
+#            [ 0,  0,  0,  0,  0,  0,  0], # board[0]
+#            [ 0,  0,  0,  0,  0,  0,  0], # board[1]
+#            [ 0,  0,  0,  0,  0,  0,  0], # board[2]
+#            [ 0,  0,  0,  0,  0,  0,  0], # board[3]
+#            [ 0,  0,  0,  0,  0,  0,  0], # board[4]
+#            [ 0,  0,  0,  0,  0,  0,  0]  # board[5]
+#           ]
+
+# def display(board)
+#   board.each { |row| p row }
+#   p "----------------------"
+# end
+
+# display(t.board)
+# t.horizontal_win?
+
+# t.update(2, 1)
+# display(t.board)
+# p t.horizontal_win?
+
+# t.update(2, 1)
+# display(t.board)
+# p t.horizontal_win?
+
+# t.update(3, 1)
+# display(t.board)
+# p t.horizontal_win?
+
+# t.update(4, 1)
+# display(t.board)
+# p t.horizontal_win?
+
+# t.update(6, 1)
+# display(t.board)
+# p t.horizontal_win?
+
+# t.update(2, 1)
+# display(t.board)
+# p t.vertical_win?
+
+# t.update(2, 1)
+# display(t.board)
+# p t.vertical_win?
+
+# t.update(3, 1)
+# display(t.board)
+# p t.vertical_win?
+
+# t.update(4, 1)
+# display(t.board)
+# p t.vertical_win?
+
+# t.update(4, 1)
+# display(t.board)
+# p t.diagonal_win?
+
+# t.update(5, 1)
+# display(t.board)
+# p t.diagonal_win?
+
+# t.update(5, 1)
+# display(t.board)
+# p t.diagonal_win?
+
+# t.update(5, 1)
+# display(t.board)
+# p t.diagonal_win?
+
+# t.update(5, 1)
+# display(t.board)
+# p t.diagonal_win?
+
