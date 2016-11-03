74bf78ad11e8b88eb7e2aec3098e048017540762
Alexa <alexa.anderson@outlook.com>
2016-11-03 10:49:21 -0500

    Refactor

--- connect_four/board.rb
+++ connect_four/board.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Board
   DIAGONAL_VECTORS = [
                        [[-3,-3], [-2,-2], [-1,-1], [0,0]],
@@ -9,32 +11,63 @@ class Board
                        [[-2,2],[-1,1],[0,0],[1,-1]],
                        [[-1,1],[0,0],[1,-1],[2,-2]],
                      ]
-                     attr_reader :columns
+  
+  attr_reader :columns
 
   def initialize
-    @redsPositions = []
-    @bluesPositions = []
-    @columns = Array.new(6) {[" "," "," "," "," "," "," "]}
+    @red_positions = []
+    @blue_positions = []
+    @columns = Array.new(7) { Array.new(6) { ' ' }}
   end
 
   def add_piece(player, col)
-    @columns[col][row] = player.symbol if valid_drop?(col)
     row = get_row(col)
+    @columns[col][row] = player.symbol if valid_drop?(col)
     position = [col, row]
     player_array(player).push position
     @last_piece = position
   end
 
+  def render
+    rows = []
+    6.times do |pos|
+      print "|"
+      @columns.each do |col|
+        print "#{col[pos]}|"
+      end
+      puts
+    end
+    puts "---------------"
+    print "|0|1|2|3|4|5|6|"
+  end
+
+  def four_in_a_row?(player)
+    win_combos = winning_combos(@last_piece)
+    puts "last piece: #{@last_piece}"
+    four_in_a_row = false
+    win_combos.each do |direction, win_combo|
+      # wincombo is array of array of vectors
+      four_in_a_row = win_combo.all? do |position|
+        player_array(player).include? position
+        binding.pry
+      end
+      break if four_in_a_row == true
+    end
+    four_in_a_row
+  end
+
+  # private
   def get_row(col)
-    row = col.length - 1
+    spaces = @columns[col].count{ |pos| pos == " " }
+    row = spaces - 1
   end
 
   def valid_drop?(col)
-    col.in_bounds? && col.not_full?
+    in_bounds?(col) && not_full?(col)
   end
 
   def not_full?(col)
-    col.length < 7
+    @columns[col].length < 7
   end
 
   def in_bounds?(col)
@@ -51,10 +84,10 @@ class Board
 
   def vertical_combos(position)
     [
-      [position,
-      [position[0], (position[1] - 1)],
-      [position[0], (position[1] - 2)],
-      [position[0], (position[1] - 3)]]
+      position,
+      [position[0], (position[1] + 1)],
+      [position[0], (position[1] + 2)],
+      [position[0], (position[1] + 3)]
     ]
   end
 
@@ -91,27 +124,6 @@ class Board
   end
 
   def player_array(player)
-    player.color == 'red' ? @redsPositions : @bluesPositions
+    player.color == 'red' ? @red_positions : @blue_positions
   end
-
-  def render
-    @columns.transpose.each do |row|
-      p row.join "|"
-    end
-    print "_____________"
-    print "0|1|2|3|4|5|6"
-  end
-
-  def four_in_a_row?(player)
-    win_combos = winning_combos(@last_piece)
-    four_in_a_row = false
-    win_combos.each do |direction, win_combo|
-      four_in_a_row = win_combo.all? do |position|
-        player_array(player).include? position
-      end
-      break if four_in_a_row == true
-    end
-    four_in_a_row
-  end
-
 end
--- connect_four/player.rb
+++ connect_four/player.rb
@@ -4,8 +4,7 @@
 # play passes color position of piece to board
 
 class Player
-  attr_reader :color
-              :symbol
+  attr_reader :color, :symbol
 
   def initialize(attrs = {})
     @color = attrs[:color]
@@ -16,6 +15,10 @@ class Player
     puts "Please type in an column number to drop your piece."
     response = gets.chomp.to_i
     response.match(/\A[0-6]\Z/) ? response : input
+  end
+
+  def symbol
+    @symbol
   end
 
 end
