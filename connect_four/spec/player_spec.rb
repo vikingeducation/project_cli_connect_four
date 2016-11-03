958abb0f33b5eb27518488048fdb86989565b644
Jake Tingley <jaket32@gmail.com>
2016-11-03 17:50:15 -0400

    added test to player_spec

--- connect_four/spec/player_spec.rb
+++ connect_four/spec/player_spec.rb
@@ -1,10 +1,25 @@
 load 'lib/player.rb'
 
 describe 'Player' do
-  let(:p){ Player.new }
+  let(:p){ Player }
+
   describe '#initialize' do
+  
     it 'raises an error when arguments are passed to initialize' do
       expect{Player.new}.to raise_error(ArgumentError)
+    end
+
+    it 'assigns a color upon initialization' do
+      expect(p.new('blue').color).to eq('blue')
+    end
+
+    it 'assigns x if player color equals red' do
+      a = p.new('red')
+      expect(a.symbol).to eq('X')
+    end
+
+    it 'assigns o to any player color other than red' do
+      expect(p.new('blue').symbol).to eq('O')
     end
   end
 end
