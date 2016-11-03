00e6ec019d139feadde773d2fefccb63632ff0ae
Jake Tingley <jaket32@gmail.com>
2016-11-03 18:57:58 -0400

    adding game_spec tests

--- connect_four/spec/game_spec.rb
+++ connect_four/spec/game_spec.rb
@@ -5,9 +5,32 @@ describe "Game" do
   describe '#current_player'do
 
     it 'returns @color == "red" if count is odd' do
-      expect(Game.new.current_player(1).color).to eq(Player.new('red').color)
+      expect(Game.new.current_player(1).color).to eq('red')
+    end
+
+    it 'returns @color == "blue" if count is even' do
+      expect(Game.new.current_player(2).color).to eq('blue')
     end
 
   end
 
+  describe '#board_full' do
+
+    it 'returns true if board is full' do
+      expect(Game.new.board_full?(44)).to be true
+    end
+
+    it 'returns false if board has open spots' do
+      expect(Game.new.board_full?(19)).to be false
+    end
+  end
+
+  describe ' ' do
+
+    it "should tell us if #play calls #game_loop" do
+      expect(subject).to receive(:bar).with("an argument I want")
+      subject.foo
+    end
+
+  end
 end
