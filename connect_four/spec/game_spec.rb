1a88e1ab70763ea2d713291d88db1b8ec81eaf47
Jonathan Popenuck <john.popenuck@gmail.com>
2016-11-03 15:38:37 -0700

    fixed game rspec

--- connect_four/spec/game_spec.rb
+++ connect_four/spec/game_spec.rb
@@ -3,9 +3,8 @@ load 'lib/game.rb'
 describe "Game" do
 
   describe '#current_player'do
-    let(:player){Player.new('red')}
-    xit 'returns @player_red if count is odd' do
-      expect(Game.new.current_player(1)).to eq(player)
+    it 'returns @color == "red" if count is odd' do
+      expect(Game.new.current_player(1).color).to eq(Player.new('red').color)
     end
 
   end
