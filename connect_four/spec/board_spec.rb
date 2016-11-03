6581b52e7160564ca866e2c9f9eaf98b993a74f6
luke-schleicher <luke.j.schleicher@gmail.com>
2016-11-03 13:52:07 -0500

    1:52

---
+++ connect_four/spec/board_spec.rb
@@ -0,0 +1,86 @@
+# class Board
+# render - skip
+# column
+# column_full?
+# place(piece_type, column)
+# four_in_a_row?
+# detected_win?
+# vertical_win?(piece_type)
+# horizontal_win?(piece_type)
+# diagonal_win?(piece_type)
+# generate_diagonals(rect_array)
+# board_full?
+
+require 'board'
+
+describe Board do
+
+  let(:grid_1) {[
+                 [' ',' ','O',' ',' ',' ',' '],
+                 [' ',' ','O',' ',' ',' ',' '],
+                 [' ',' ','X','O','O','X','X'],
+                 [' ',' ','O','X','O','O','X'],
+                 [' ',' ','O','X','X','X','O'],
+                 ['X','O','O','X','O','X','X']
+               ]}
+
+  let(:board_1){Board.new(grid_1)}
+
+  describe '#column' do
+    it 'takes a column_index as a parameter, flips the board on its side, and returns the column' do
+      expect(board_1.column(2)).to eq(["O", "O", "X", "O", "O", "O"])
+    end
+  end
+
+
+
+
+# column: it 'takes a column_index as a parameter, flips the board on its side, and returns the column'
+#   board = Board.new([
+#     [' ',' ','O',' ',' ',' ',' '],
+#     [' ',' ','O',' ',' ',' ',' '],
+#     [' ',' ','X','O','O','X','X'],
+#     [' ',' ','O','X','O','O','X'],
+#     [' ',' ','O','X','X','X','O'],
+#     ['X','O','O','X','O','X','X']
+#   ])
+#   board.column(2) =>
+#     ["O", "O", "X", "O", "O", "O"]
+
+# column_full?: 
+#   it 'takes a column_index and returns true if that column_index is full'
+#   it 'takes a column_index and returns false if that column_index is not full'
+
+# place:
+#   it 'takes a piece_type and a column_index and adds that piece to the first empty space in that column'
+
+# four_in_a_row?:
+#   it 'checks if there are 4 in a row of piece_type in a long array'
+#   it 'checks if there are 4 in a row of piece_type in a short array'
+#   it 'checks if there are 4 in a row of piece_type in an array of length 4'
+
+# detected_win?:
+
+# horizontal_win?:
+#   it 'finds a horizontal win in a winning grid'
+#   it 'does not find a horizontal win in a losing grid'
+
+# vertical_win?:
+#   it 'finds a vertical win in a winning grid'
+#   it 'does not find a vertical win in a losing grid'
+
+# diagonal_win?:
+#   it 'finds a diagonal win in a winning grid'
+#   it 'does not find a diagonal win in a losing grid'
+
+# generate_diagonals:
+#   it 'lists all the diagonals in a square array'
+#   it 'lists all the diagonals in a rectangular array'
+
+# board_full?:
+#   it 'returns true if the board is full'
+#   it 'returns false if the board is not full'
+
+end
+
+
