c0ec91a36dc36bf88caed6f24bd2d18e7199cce8
andie5 <andieasa@googlemail.com>
2017-01-05 20:41:44 +0000

    removed blank lines

--- connect_four/board.rb
+++ connect_four/board.rb
@@ -107,51 +107,7 @@ module Connect_Four
                 end
                 counter2.map! { |value| value -= 1 }
             end
-            
-             
             diagonals
-             
-            
-            # @board[0][6], @board[1][5], @board[2][4], @board[3][3]
-            # @board[1][6], @board[2][5], @board[3][4], @board[4][3]  
-            # @board[2][6], @board[3][5], @board[4][4], @board[5][3]
-
-
-            # @board[0][5], @board[1][4], @board[2][3], @board[3][2]
-            # @board[1][5], @board[2][4], @board[3][3], @board[4][2]
-            # @board[2][5], @board[3][4], @board[4][3], @board[5][2]
-
-
-            # @board[0][4], @board[1][3], @board[2][2], @board[3][1]
-            # @board[1][4], @board[2][3], @board[3][2], @board[4][1]
-            # @board[2][4], @board[3][3], @board[4][2], @board[5][1] 
-
-
-            # @board[0][3], @board[1][2], @board[2][1], @board[3][0] 
-            # @board[1][3], @board[2][2], @board[3][1], @board[4][0]
-            # @board[2][3], @board[3][2], @board[4][1], @board[5][0]
-            
-
-
-
-
-             # [
-             # [ @board[2][0],@board[3][1],@board[4][2],@board[5][3] ],
-             # [ @board[2][1],@board[3][2],@board[4][3],@board[5][4] ],
-             # [ @board[2][2],@board[3][3],@board[4][4],@board[5][5] ],
-             # [ @board[2][3],@board[3][4],@board[4][5],@board[5][6] ],
-            
-             
-             # [ @board[1][0],@board[2][1],@board[3][2],@board[4][3] ],
-             # [ @board[1][1],@board[2][2],@board[3][3],@board[4][4] ],
-             # [ @board[1][2],@board[2][3],@board[3][4],@board[4][5] ],
-             # [ @board[1][3],@board[2][4],@board[3][5],@board[4][6] ],
-
-             # [ @board[0][0],@board[1][1],@board[2][2],@board[3][3] ],
-             # [ @board[0][1],@board[1][2],@board[2][3],@board[3][4] ],
-             # [ @board[0][2],@board[1][3],@board[2][4],@board[3][5] ],
-             # [ @board[0][3],@board[1][4],@board[2][5],@board[3][6] ]
-             # ]
         end
 
         # vertical combinations
@@ -166,18 +122,6 @@ module Connect_Four
                 end
                 counter.map! { |value| value += 1 }
             end
-
-            # 7.times do |i|
-            #     verticals << [@board[0][i],@board[1][i],@board[2][i], @board[3][i]]
-            # end
-
-            # 7.times do |i|
-            #     verticals << [@board[1][i],@board[2][i],@board[3][i], @board[4][i]]
-            # end
-
-            # 7.times do |i|
-            #     verticals << [@board[2][i],@board[3][i],@board[4][i], @board[5][i]]
-            # end
             verticals
         end
 
@@ -193,22 +137,6 @@ module Connect_Four
                 end
                 counter.map! { |value| value += 1 }
             end
-
-            # 6.times do |i|
-            #     horizontals << [@board[i][0],@board[i][1],@board[i][2], @board[i][3]]
-            # end
-
-            # 6.times do |i|
-            #     horizontals << [@board[i][1],@board[i][2],@board[i][3], @board[i][4]]
-            # end
-
-            # 6.times do |i|
-            #     horizontals << [@board[i][2],@board[i][3],@board[i][4], @board[i][5]]
-            # end
-
-            # 6.times do |i|
-            #     horizontals << [@board[i][3],@board[i][4],@board[i][5], @board[i][6]]
-            # end
             horizontals  
         end
 
