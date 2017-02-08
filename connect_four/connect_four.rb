1559e584de2dc5a6fd6817e58d23ddec4346a78c
Robert Wayne <robertjwayne@gmail.com>
2017-02-07 23:11:20 -0500

    Add initialize method

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,30 +1,38 @@
 # Controls the game play
 class ConnectFour
 # initialize
-  # set up the board
-  # set up the players
-  # assign the starting player
+  def initialize
+    # set up the board
+    @board = Board.new
+
+    # set up the players
+    @player_x = Player.new("Player 1", :x, @board)
+    @player_y = Player.new("Player 2", :y, @board)
+
+    # assign the starting player
+    @current_player = @player_x
+  end
 
 # play
-  # loop infinitely
-    # call the board rendering method
-    # ask for coordinates from the current player
-    # break the loop IF the game is over
-    # switch players
+# loop infinitely
+# call the board rendering method
+# ask for coordinates from the current player
+# break the loop IF the game is over
+# switch players
 
 # check_game_over
-  # check_victory
-  # check_draw
+# check_victory
+# check_draw
 
 # check_victory
-  # IF board says current player's piece has
-  # a winning_combination?
-  # display a victory message
+# IF board says current player's piece has
+# a winning_combination?
+# display a victory message
 
 # check_draw
-  # IF board says it's full
-  # display a draw message
+# IF board says it's full
+# display a draw message
 
 # switch_players
-  # PlayerX >> PlayerO or vice versa
+# PlayerX >> PlayerO or vice versa
 end
