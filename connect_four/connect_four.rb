640bf2ed78a919f8cd82ac7e7f6aedcf2d7f84eb
efraimmgon <efraimmaugusto@gmail.com>
2017-02-13 11:09:56 -0300

    brainstorm basic workflow

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,1 +1,31 @@
-# Your code here!
+# ------------------------------------------------------------
+# Task
+# ------------------------------------------------------------
+
+# Your task is to build a command line Connect Four game. It should start
+# with two players and render the board to the Command Line between turns.
+# The board does not need to look pretty, just give enough information so
+# you know its layout.
+
+# You should validate user inputs and determine victory/loss/draw as
+# dictated by the rules.
+
+# ------------------------------------------------------------
+# code
+# ------------------------------------------------------------
+
+# 2 players
+# render board between turns
+# prompt input
+# validate user input
+# win?
+# lose?
+
+# win: get 4 checkers of their color positioned either horizontally,
+# diagonally, or vertically in a line
+
+class ConnectFour
+
+
+
+end
