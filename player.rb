14a6a0e14fe4cba75690bb6fba30301f26760089
Roy Chen <roychenlei@gmail.com>
2017-02-15 12:41:59 +0800

    initial methods and constructor

--- player.rb
+++ player.rb
@@ -1,6 +1,17 @@
 module ConnectFour
 
   class Player
+    attr_reader :name,
+                :marker
+
+    def initialize(name, marker)
+      @name = name
+      @marker = marker
+    end
+
+    # asks player which column to place his marker in
+    def ask_for_move
+    end
   end
 
 end
