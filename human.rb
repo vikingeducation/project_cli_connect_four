efa99f86be81893138d060c1ceca53958082b5da
Roy Chen <roychenlei@gmail.com>
2017-02-17 10:25:33 +0800

    modified choose_column method to allow user to input 'q' to quit

--- human.rb
+++ human.rb
@@ -8,13 +8,19 @@ module ConnectFour
     # asks human player which column to place his marker in
     def choose_column
       print "Please enter the column you want to place your marker in: "
+      column = gets.chomp
 
-      column = gets.chomp.to_i
-      until (1..7).include?(column)
-        print "That is an invalid column. Please enter a number from 1 to 7: "
-        column = gets.chomp.to_i
+      # allow player to quit game by entering 'q'
+      if column == "q"
+        return column
+      else
+        column = column.to_i
+        until (1..7).include?(column)
+          print "That is an invalid column. Please enter a number from 1 to 7: "
+          column = gets.chomp
+        end
       end
-
+      
       column
     end
   end
