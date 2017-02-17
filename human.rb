9189b9488de1f657d9b5b6574b414dd29a225d66
Roy Chen <roychenlei@gmail.com>
2017-02-17 09:37:06 +0800

    moved ask_for_column method to Human class

--- human.rb
+++ human.rb
@@ -4,6 +4,18 @@ module ConnectFour
     def initialize(name, marker)
       super
     end
-  end
 
+    # asks human player which column to place his marker in
+    def ask_for_column
+      print "Please enter the column you want to place your marker in: "
+
+      column = gets.chomp.to_i
+      until (1..7).include?(column)
+        print "That is an invalid column. Please enter a number from 1 to 7: "
+        column = gets.chomp.to_i
+      end
+
+      column
+    end
+  end
 end
--- player.rb
+++ player.rb
@@ -8,18 +8,5 @@ module ConnectFour
       @name = name
       @marker = marker
     end
-
-    # asks player which column to place his marker in
-    def ask_for_column
-      print "Please enter the column you want to place your marker in: "
-
-      column = gets.chomp.to_i
-      until (1..7).include?(column)
-        print "That is an invalid column. Please enter a number from 1 to 7: "
-        column = gets.chomp.to_i
-      end
-
-      column
-    end
   end
 end
