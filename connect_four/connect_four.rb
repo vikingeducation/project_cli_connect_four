ef74e60e4dceddb4593e4abca54501948b06348d
efraimmgon <efraimmaugusto@gmail.com>
2017-02-20 07:12:53 -0400

    remove unnecessary comments

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -1,28 +1,3 @@
-# ------------------------------------------------------------
-# Task
-# ------------------------------------------------------------
-
-# Your task is to build a command line Connect Four game. It should start
-# with two players and render the board to the Command Line between turns.
-# The board does not need to look pretty, just give enough information so
-# you know its layout.
-
-# You should validate user inputs and determine victory/loss/draw as
-# dictated by the rules.
-
-# ------------------------------------------------------------
-# code
-# ------------------------------------------------------------
-
-# 2 players [ok]
-# render board between turns [ok]
-# prompt input
-# validate user input
-# win?
-# lose?
-
-# win: get 4 checkers of their color positioned either horizontally,
-# diagonally, or vertically in a line
 
 class ConnectFour
   attr_accessor :app_state
@@ -297,3 +272,5 @@ class ConnectFour
   end
 
 end
+
+fuck
