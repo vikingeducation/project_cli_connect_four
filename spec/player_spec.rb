65d1d6f263008ec1aba319d3a2743a44a0d16bbb
Roy Chen <roychenlei@gmail.com>
2017-02-25 22:34:30 +0800

    defined initial spec file

---
+++ spec/player_spec.rb
@@ -0,0 +1,21 @@
+# spec/player_spec.rb
+require 'player'
+require 'grid'
+include ConnectFour
+
+describe "Player" do
+  describe "#initialize" do
+  end
+
+  describe "#name" do
+  end
+
+  describe "#marker" do
+  end
+
+  describe "#grid" do
+  end
+
+  describe "#grid=" do
+  end
+end
