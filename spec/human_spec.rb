a302b56285a91f566193e32f8c8ce5015ed6fd3b
Roy Chen <roychenlei@gmail.com>
2017-02-26 05:47:23 +0800

    removed redundant specs that would be tested in parent class, DRY

--- spec/human_spec.rb
+++ spec/human_spec.rb
@@ -5,29 +5,7 @@ require 'human'
 include ConnectFour
 
 describe "Human" do
-  describe "#initialize" do
-    it "saves passed-in values as instance variables" do
-      name = "Bob"
-      marker = "X"
-      player = Player.new(name, marker)
-
-      expect(player.name).to eq(name)
-      expect(player.marker).to eq(marker)
-    end
-
-    it "sets the @grid instance variable to nil" do
-      player = Player.new("Alice", "O")
-
-      expect(player.grid).to be_nil
-    end
-
-    it "raises an ArgumentError if the number of arguments are wrong" do
-      expect { Player.new }.to raise_error(ArgumentError)
-      expect { Player.new("Alice") }.to raise_error(ArgumentError)
-    end
-  end
-
   describe "#choose_column" do
-    
+
   end
 end
