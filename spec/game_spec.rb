5837979614069e5679eddb1c96af04d996c8120b
Roy Chen <roychenlei@gmail.com>
2017-02-26 21:11:31 +0800

    added specs for #draw? and #quit

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -21,4 +21,42 @@ describe "Game" do
       expect(game.current_player).to be_nil
     end
   end
+
+  describe "draw?" do
+    it "returns true if the game grid's 42 squares are all filled" do
+      full_grid = { 1 => ["X", "X", "X", "X", "X", "X"],
+                    2 => ["X", "X", "X", "X", "X", "X"],
+                    3 => ["X", "X", "X", "X", "X", "X"],
+                    4 => ["X", "X", "X", "X", "X", "X"],
+                    5 => ["X", "X", "X", "X", "X", "X"],
+                    6 => ["X", "X", "X", "X", "X", "X"],
+                    7 => ["X", "X", "X", "X", "X", "X"]
+                  }
+
+      expect(Game.new(full_grid).draw?).to be_truthy
+    end
+
+    it "returns false if not all the game grid's 42 squares are filled" do
+      almost_full_grid = { 1 => ["X", "X", "X", "X", "X", "X"],
+                           2 => ["X", "X", "X", "X", "X", "X"],
+                           3 => ["X", "X", "X", "X", "X", "X"],
+                           4 => ["X", "X", "X", "X", "X", "X"],
+                           5 => ["X", "X", "X", "X", "X", "X"],
+                           6 => ["X", "X", "X", "X", "X", "X"],
+                           7 => ["X", "X", "X", "X", "X"]
+                          }
+
+      expect(Game.new(almost_full_grid).draw?).to be_falsey
+    end
+
+    it "returns false if the game grid is empty" do
+      expect(game.draw?).to be_falsey
+    end
+  end
+
+  describe "#quit" do
+    it "raises a SystemExit error and exits the game" do
+      expect { game.quit }.to raise_error(SystemExit)
+    end
+  end
 end
