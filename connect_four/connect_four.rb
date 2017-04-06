ca38348d2b8dad4e0fb327223fb65e28d29cfa24
Brent Guffens <brentguf@users.noreply.github.com>
2017-04-06 17:52:31 +0200

    backup

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -7,8 +7,8 @@ class ConnectFour
 
     @grid = Grid.new
 
-    @player_1 = Player.new("Player 1", :x, @grid)
-    @player_2 = Player.new("Player 2", :o, @grid)
+    @player_1 = Player.new("Player 1", "X", @grid)
+    @player_2 = Player.new("Player 2", "O", @grid)
 
     @current_player = @player_1
 
@@ -17,16 +17,14 @@ class ConnectFour
   def play
 
     instructions
-    
     loop do 
-      
-      @grid.render
-      @current_player.get_choice
-
+      @grid.render     
+      @current_player.get_choice 
+ 
       break if game_over?
 
       switch_players
-    	
+
     end
 
   end
@@ -49,13 +47,17 @@ class ConnectFour
   end
 
   def game_over?
+
     winner? || draw?
+
   end
 
   def winner?
+
     vertical_winner?
     horizontal_winner?
     diagonal_winner?
+
   end
 
   def draw?
@@ -94,11 +96,13 @@ class ConnectFour
   end
 
   def switch_players
-    if @current_player = @player_1
+
+    if @current_player == @player_1
     	@current_player = @player_2
     else
       @current_player = @player_1
     end
+
   end
 
 end
@@ -106,37 +110,39 @@ end
 class Player
 
   def initialize(name = "Anonymous", disc, grid)
-
-		raise "Disc must be a Symbol!" unless disc.is_a?(Symbol)
     
     @name = name 
     @disc = disc
     @grid = grid
+
   end
   
   def get_choice
 
     loop do
+      column = ask_for_column - 1
 
-      column = ask_for_column
-
-      break if valid_column_number?(column) && @grid.add_piece(column - 1, @disc)
-
+      break if valid_column_number?(column) && @grid.add_piece(column, @disc)
     end
+
   end
+
   def ask_for_column
     
     puts
     puts "#{@name}, enter the column (1-7) that you want to put your disc (#{@disc}) in."
     gets.strip.to_i
+
   end
+
   def valid_column_number?(column)
     
-    if (1..7).include?(column)
+    if (0..6).include?(column)
     	true
     else
     	puts "Your column of choice is not within the allowed range (1-7)."
     end
+
   end
 
 end
@@ -147,38 +153,47 @@ class Grid
 
   def initialize 
 
-    @grid_arr = Array.new(6, Array.new(7))
-	
+    @grid_arr = Array.new(7, Array.new(6))
+
   end
 
   def render
+    columns = @grid_arr.length - 1
 
-    @grid_arr.each do |row|
-      row.each do |cell|
-        cell.nil? ? print(" - ") : print(" #{cell} ")
+    while columns > 0
+      @grid_arr.each do |row|
+        row[columns].nil? ? print(" - ") : print(" #{row[columns]} ")
       end
       puts
+      columns -= 1 
     end
+
     print " =================== "
     puts
 
   end
 
   def add_piece(column, disc)
-    
-    if valid_destination?(column)
-      
-      destination_row = @grid_arr[column].index{|x| x.nil?}
-      
-      @grid_arr[column][destination_row] = disc
 
+    if destination_available?(column)
+      @grid_arr[column].shift
+      @grid_arr[column].unshift(disc)
+      print @grid_arr
+      puts
+      true
+    else
+      false
     end
 
   end
 
-  def valid_destination?(column)
-    
-    @grid_arr[column].last.nil? 
+  def destination_available?(column)
+
+    if @grid_arr[column].last.nil?
+      true
+    else
+      false
+    end
 
   end
 
