e11bb730f6f092fb1d655b8f709b78e6fd38022d
Sean Luckett <whitecrane007@gmail.com>
2017-04-08 13:47:08 -0600

    Add UI class

---
+++ connect_four/connect_four_presenter.rb
@@ -0,0 +1,28 @@
+class ConnectFourPresenter
+  def display_grid(grid)
+    grid.to_s
+  end
+
+  def display_tie(grid)
+    puts 'Tie game'
+    display_grid(grid)
+  end
+
+  def display_winner(player, grid)
+    puts "#{player} won! Here's the grid:"
+    display_grid(grid)
+  end
+
+  def prompt_player_move
+    print 'Choose a column 1 - 7: '
+    gets.chomp
+  end
+
+  def rules
+    puts 'Rules message here.'
+  end
+
+  def welcome
+    puts 'Welcome message here.'
+  end
+end
