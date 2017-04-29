cea0fdf9369dac847e135bd605cffb955318ca2d
Miguel de Luis Espinosa <luis.miguelde@gmail.com>
2017-04-29 17:47:09 +0100

    ai

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -12,7 +12,7 @@ class C4_space
   def set(new_state)
     raise(@@unknown_state_mssg) unless @@states.include?(new_state)
     # this should only happen, if at all, in development
-    if @state != :empty
+    if @state != :empty && new_state != :empty
       false # a space with a disc does not accept a new disc
     else
       @state = new_state
@@ -45,6 +45,7 @@ class C4_board
   end
 
   def render
+    puts "\e[H\e[2J\n"
     puts "\t  1 2 3 4 5 6 7"
     j = 1
     @board.each{ |y|
@@ -72,6 +73,7 @@ class C4_board
   def drop(column, player)
     if column_full?(column)
       puts("Column full")
+      false
     else
       i = 6
       while i > 0 && space(column, i).state != :empty
@@ -82,11 +84,15 @@ class C4_board
     end
   end
 
+  def undrop
+    set(@last_col, @last_row, :empty)
+  end
+
   def column_win?(player)
     i = @last_row + 1
     total = 1
-    while i <= 6 do
-      total += 1 if space(@last_col, i).state == player
+    while i <= 6 && space(@last_col, i).state == player do
+      total += 1
       i += 1
     end
     true if total > 3
@@ -112,7 +118,6 @@ class C4_board
   def f_diagonal_win?(player)
     x = @last_row + 1
     y = @last_col + 1
-    puts("x", x,"y", y,"s")
     total = 1
     # search downwards
     while y < 6 && x < 7 && space(y, x).state == player do
@@ -168,8 +173,9 @@ class C4_board
 end
 
 class Player
-  def initialize
+  def initialize(two_players)
     @player = :yellow
+    @two_players = two_players
   end
 
   def switch
@@ -180,13 +186,46 @@ class Player
     @player
   end
 
-  def ask_input
-    puts( "#{@player}, choose a column to drop your stone")
-    col = gets.chomp.to_i
-    if col < 1 || col > 7
-      ask_input
+  def win?(game, player)
+    col = 1
+    while col < 7 do
+      game.drop(col, player)
+      if game.won?(player)
+        game.undrop
+        found = col
+        break
+      else
+        game.undrop
+        col += 1
+        found = false
+      end
+    end
+    found
+  end
+
+  def ai_input(game)
+    play_to_win = win?(game, :yellow)
+    play_defense = win?(game, :red)
+    if play_to_win
+      play_to_win
+    elsif play_defense
+      play_defense
     else
-      col
+      [1,2,3,4,5,6,7].sample
+    end
+  end
+
+  def ask_input(game)
+    if @two_players && @player == :yellow
+      ai_input(game)
+    else
+      puts( "#{@player}, choose a column to drop your stone")
+      col = gets.chomp.to_i
+      if col < 1 || col > 7
+        ask_input(game)
+      else
+        col
+      end
     end
   end
 end
@@ -194,13 +233,28 @@ end
 
 
 game = C4_board.new
-player = Player.new
+if ARGV[0] == "2"
+  two_players = true
+else
+  two_players = false
+end
+ARGV.clear
+
+player = Player.new(two_players)
+dropped = true
+turns = 0
 
 while !game.won?(player.who) do
-  player.switch
+  break if turns > 42
+  player.switch unless dropped
   game.render
-  game.drop(player.ask_input, player.who)
+  dropped = game.drop(player.ask_input(game), player.who)
+  turns += 1
 end
 
 game.render
-puts("#{player.who} won!")
+if turns > 42
+  puts("It's a draw")
+else
+  puts("#{player.who} won!")
+end
