3449715d2783989870acc17bc9537e3208850035
mac718 <mac718@gmail.com>
2017-09-26 21:23:41 -0400

    created files for classes

--- connect_four/board.rb
+++ connect_four/board.rb
@@ -2,12 +2,13 @@ class Board
   attr_accessor :board, :red_piece, :black_piece
 
   def initialize
-    @board = [['-', '-', '-', '-', '-', '-', '-'],
+    @board = [['1', '2', '3', '4', '5', '6', '7'],
               ['-', '-', '-', '-', '-', '-', '-'],
               ['-', '-', '-', '-', '-', '-', '-'],
               ['-', '-', '-', '-', '-', '-', '-'],
               ['-', '-', '-', '-', '-', '-', '-'],
-              ['-', '-', '-', '-', '-', '-', '-']]
+              ['-', '-', '-', '-', '-', '-', '-'],
+              ['-', '-', 'X', '-', '-', '-', '-']]
   end
 
   def display_board
@@ -16,6 +17,22 @@ class Board
       print "\n"
     end
   end
+
+  def update_board(column)
+    row = 1
+    while self.board[row][column - 1] == '-' && row < 7
+      row += 1
+    end
+    puts row
+    if row == 7
+      self.board[6][column - 1] = 'X'
+    else
+      self.board[row - 1][column - 1] = 'X'
+    end
+  end
 end
 
-Board.new.display_board
+a = Board.new
+
+a.update_board(3)
+a.display_board
--- connect_four/game.rb
+++ connect_four/game.rb
@@ -1,2 +1,20 @@
+require_relative 'human_player'
+require_relative 'computer_player'
+
 class Game
+  def greeting
+    puts "**Welcome to Connect Four!"
+    puts "Enter 1 for a one player game or 2 for a two player game."
+  end
+
+  def create_players
+    game_type = gets.chomp.to_i
+    if game_type == 1
+      player_1 = HumanPlayer.new
+      player_2 = ComuterPlayer.new
+    else
+      player_1 = HumanPlayer.new
+      player_2 = HumanPlayer.new
+    end
+  end
 end
