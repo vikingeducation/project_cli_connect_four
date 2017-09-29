1235ed1758333a324aaeb42dabfee2a199de8f6b
mac718 <mac718@gmail.com>
2017-09-29 17:41:43 -0400

    updated basic computer player AI

--- connect_four/computer_player.rb
+++ connect_four/computer_player.rb
@@ -3,10 +3,12 @@ class ComputerPlayer
   def almost_win_horizontal?(current_player_piece, board)
     board.each_with_index do |row, row_index|
       count = 0
-      row.each do |position|
+      row.each_with_index do |position, position_index|
         if position == current_player_piece
           count += 1
           return row_index + 1 if count == 3
+        elsif position == '-' && row[position_index + 1] == current_player_piece
+          next
         else
           count = 0
         end
@@ -17,10 +19,12 @@ class ComputerPlayer
   def almost_win_vertical?(current_player_piece, board)
     board.transpose.each_with_index do |row, row_index|
       count = 0
-      row.each do |position|
+      row.each_with_index do |position, position_index|
         if position == current_player_piece
           count += 1
           return row_index + 1 if count == 3
+        elsif position == '-' && row[position_index + 1] == current_player_piece
+          next
         else
           count = 0
         end
@@ -30,12 +34,14 @@ class ComputerPlayer
   end
 
   def almost_win_diagonal?(current_player_piece, baord)
-    @diagonals.each do |diagonal|
+    @diagonals.each_with_index do |diagonal, diagonal_index|
       count = 0
-      diagonal.each do |position|
+      diagonal.each_with_index do |position, position_index|
         if position == current_player_piece
           count += 1
           return row_index + 1 if count == 3
+        elsif position == '-' && row[position_index + 1] == current_player_piece
+          next
         else
          count = 0
         end
@@ -52,7 +58,7 @@ class ComputerPlayer
 
   def make_move(current_player_piece, board)
     if almost_win?(current_player_piece, board)
-      almost_win?(current_player_piece, board)
+      almost_win?(current_player_piece, board) 
     else
       sleep 1
       (1..7).to_a.sample
