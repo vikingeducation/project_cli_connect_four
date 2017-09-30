f7ddd1c5e91b386df45871df290265199a193889
mac718 <mac718@gmail.com>
2017-09-30 10:54:53 -0400

    added move validation to HumanPlayer

--- connect_four/human_player.rb
+++ connect_four/human_player.rb
@@ -1,5 +1,14 @@
 class HumanPlayer 
   def make_move(current_player_piece, board, diagonals)
-    column = gets.chomp.to_i
+    @column = gets.chomp.to_i
+    validate_move(current_player_piece, board, diagonals)
   end
+
+  def validate_move(current_player_piece, board, diagonals)
+    until (1..7).include?(@column)
+      puts "Out of bounds. Please select a number between 1 and 7."
+      make_move(current_player_piece, board, diagonals)
+    end
+    @column
+  end 
 end
