8976e5a8dff6f4d46612575a426283897266cee4
mac718 <mac718@gmail.com>
2017-10-03 23:03:01 -0400

    more tests for Board

--- connect_four/lib/board.rb
+++ connect_four/lib/board.rb
@@ -15,17 +15,17 @@ class Board
 
   def set_diagonals
     self.diagonals = [[board[3][0], board[4][1], board[5][2], board[6][3]],
-                    [board[2][0], board[3][1], board[4][2], board[5][3], board[6][4]],
-                    [board[1][0], board[2][1], board[3][2], board[4][3], board[5][4], board[6][5]],
-                    [board[1][1], board[2][2], board[3][3], board[4][4], board[5][5], board[6][6]],
-                    [board[1][2], board[2][3], board[3][4], board[4][5], board[5][6]],
-                    [board[1][3], board[2][4], board[3][5], board[4][6]],
-                    [board[3][6], board[4][5], board[5][4], board[6][3]],
-                    [board[2][6], board[3][5], board[4][4], board[5][3], board[6][2]],
-                    [board[1][6], board[2][5], board[3][4], board[4][3], board[5][2], board[6][1]],
-                    [board[1][5], board[2][4], board[3][3], board[4][2], board[5][1], board[6][0]],
-                    [board[1][4], board[2][3], board[3][2], board[4][1], board[5][0]],
-                    [board[1][3], board[2][2], board[3][1], board[4][0]]]
+                      [board[2][0], board[3][1], board[4][2], board[5][3], board[6][4]],
+                      [board[1][0], board[2][1], board[3][2], board[4][3], board[5][4], board[6][5]],
+                      [board[1][1], board[2][2], board[3][3], board[4][4], board[5][5], board[6][6]],
+                      [board[1][2], board[2][3], board[3][4], board[4][5], board[5][6]],
+                      [board[1][3], board[2][4], board[3][5], board[4][6]],
+                      [board[3][6], board[4][5], board[5][4], board[6][3]],
+                      [board[2][6], board[3][5], board[4][4], board[5][3], board[6][2]],
+                      [board[1][6], board[2][5], board[3][4], board[4][3], board[5][2], board[6][1]],
+                      [board[1][5], board[2][4], board[3][3], board[4][2], board[5][1], board[6][0]],
+                      [board[1][4], board[2][3], board[3][2], board[4][1], board[5][0]],
+                      [board[1][3], board[2][2], board[3][1], board[4][0]]]
   end   
 
   def display_board
--- connect_four/spec/board_spec.rb
+++ connect_four/spec/board_spec.rb
@@ -21,10 +21,10 @@ describe Board do
     end
   end
 
-  describe "#win_horizontal" do 
-    context "four of the same game piece in a row on the board" do 
+  describe "#win_horizontal?" do 
+    let(:current_player_piece) { 'R' }
+    context "four of the same game piece occupy conesecutive spaces in a row on the board" do 
       it "returns true" do 
-        current_player_piece = "R"
         board.board = [['1', '2', '3', '4', '5', '6', '7'],
                       ['-', '-', '-', '-', '-', '-', '-'],
                       ['-', '-', '-', '-', '-', '-', '-'],
@@ -32,8 +32,159 @@ describe Board do
                       ['-', '-', '-', '-', '-', '-', '-'],
                       ['-', '-', 'R', 'R', 'R', 'R', '-'],
                       ['-', '-', '-', '-', '-', '-', '-']] 
-        expect(board.win_horizontal?(current_player_piece)).to be_truthy
+        expect(board.win_horizontal?(current_player_piece)).to be true
       end
+    end
+
+    context "fewer than four of the same game piece occupy conesecutive spaces in a row on the board" do 
+      it "returns false" do 
+        board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', 'R', 'R', 'R', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-']] 
+        expect(board.win_horizontal?(current_player_piece)).to be false
+      end
+    end
+  end
+
+  describe "#win_vertical?" do 
+    let(:current_player_piece) { 'R' }
+    context "four of the same game piece occupy conesecutive spaces in a column on the board" do 
+      it "returns true" do 
+        board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', '-', '-', 'R', '-'],
+                      ['-', '-', '-', '-', '-', 'R', '-'],
+                      ['-', '-', '-', '-', '-', 'R', '-'],
+                      ['-', '-', '-', '-', '-', 'R', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-']] 
+        expect(board.win_vertical?(current_player_piece)).to be true
+      end
+    end
+
+    context "fewer than four of the same game piece occupy conesecutive spaces in a column on the board" do 
+      it "returns false" do 
+        board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', '-', '-', 'R', '-'],
+                      ['-', '-', '-', '-', '-', 'R', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-']] 
+        expect(board.win_vertical?(current_player_piece)).to be false
+      end
+    end
+  end
+
+    describe "#win_diagonal?" do 
+    let(:current_player_piece) { 'R' }
+    context "four of the same game piece occupy conesecutive spaces in a diagonal on the board" do 
+      it "returns true" do 
+        board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', 'R', '-', '-', '-', '-'],
+                      ['-', '-', '-', 'R', '-', '-', '-'],
+                      ['-', '-', '-', '-', 'R', '-', '-'],
+                      ['-', '-', '-', '-', '-', 'R', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-']]
+        board.set_diagonals 
+        expect(board.win_diagonal?(current_player_piece)).to be true
+      end
+    end
+
+    it "returns true" do 
+      board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                    ['-', '-', '-', '-', '-', '-', '-'],
+                    ['-', '-', '-', '-', 'R', '-', '-'],
+                    ['-', '-', '-', 'R', '-', '-', '-'],
+                    ['-', '-', 'R', '-', '-', '-', '-'],
+                    ['-', 'R', '-', '-', '-', '-', '-'],
+                    ['-', '-', '-', '-', '-', '-', '-']]
+      board.set_diagonals 
+      expect(board.win_diagonal?(current_player_piece)).to be true
+    end
+
+    context "fewer than four of the same game piece occupy conesecutive spaces in a diagonal on the board" do 
+      it "returns false" do 
+        board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-'],
+                      ['-', '-', '-', 'R', '-', '-', '-'],
+                      ['-', '-', '-', '-', 'R', '-', '-'],
+                      ['-', '-', '-', '-', '-', 'R', '-'],
+                      ['-', '-', '-', '-', '-', '-', '-']]
+        board.set_diagonals  
+        expect(board.win_diagonal?(current_player_piece)).to be false
+      end
+    end
+  end
+
+  describe "#full?" do 
+    context "Board is full" do 
+      it "returns true" do 
+        board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R']]
+        expect(board.full?).to be true
+      end
+    end
+
+    context "Bord is not full" do
+      it "returns false" do
+        board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                      ['-', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R'],
+                      ['R', 'R', 'R', 'R', 'R', 'R', 'R']]
+        expect(board.full?).to be false
+      end
+    end
+  end
+
+  describe "#update_board" do 
+    let(:current_player_piece) { 'R' }
+    context "Board is empty" do 
+      it "returns 6" do 
+        column = 6
+        expect(board.update_board(column, current_player_piece)).to eql(6)
+      end
+    end
+
+    context "Bottom two rows of specified column are occupied." do 
+      it "returns 4" do 
+        column = 6
+        board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', 'R', '-'],
+                       ['-', '-', '-', '-', '-', 'R', '-']]
+        expect(board.update_board(column, current_player_piece)).to eql(4)
+      end
+    end
+  end
+
+  describe "#set_diagonals" do 
+    it "updates the values of @diagonals after the board is updated" do 
+      board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', '-', '-', '-'],
+                       ['-', '-', '-', '-', 'R', '-', '-'],
+                       ['-', '-', '-', '-', '-', 'R', '-']]
+      board.set_diagonals
+      expect(board.diagonals[2]).to eql(['-', '-', '-', '-', 'R', 'R'])
     end
   end
 end
