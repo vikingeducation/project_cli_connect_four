eeedc409368f7dafc4f5d09400361720eb04e7db
mac718 <mac718@gmail.com>
2017-10-06 17:16:35 -0400

    added attr_accessor for @column in HumanPlayer

--- connect_four/lib/human_player.rb
+++ connect_four/lib/human_player.rb
@@ -1,16 +1,18 @@
 class HumanPlayer 
+  attr_accessor :column
+
   def make_move(current_player_piece, board, diagonals)
-    @column = gets.chomp.to_i
+    self.column = gets.chomp.to_i
     validate_move(current_player_piece, board, diagonals)
   end
 
   private 
-  
+
   def validate_move(current_player_piece, board, diagonals)
-    until (1..7).include?(@column)
+    until (1..7).include?(column)
       puts "Out of bounds. Please select a number between 1 and 7."
       make_move(current_player_piece, board, diagonals)
     end
-    @column
+    column
   end 
 end
