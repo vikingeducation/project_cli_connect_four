b6f5013476206a7120e865bdaa85623101ca2a73
mac718 <mac718@gmail.com>
2017-10-06 15:20:20 -0400

    Added more tests for methods in ComputerPlayer

--- connect_four/spec/computer_player_spec.rb
+++ connect_four/spec/computer_player_spec.rb
@@ -37,7 +37,7 @@ describe ComputerPlayer do
   end
 
   describe "#almost_win_diagonal?" do 
-    context "there is a row on the board with three computer game pieces in consecutive postitions" do 
+    context "there is a diagonal on the board with three computer game pieces in consecutive postitions" do 
       it "returns the index of the column that would result in a win + 1" do 
         board.board = [['1', '2', '3', '4', '5', '6', '7'],
                        ['-', '-', '-', '-', '-', '-', '-'],
@@ -63,22 +63,83 @@ describe ComputerPlayer do
         board.set_diagonals
         expect(player.almost_win_diagonal?(current_player_piece, board.diagonals, board.board)).to eql(6)
       end
-    end
-=begin
-    context "there is a row on the board with 4-space unit containing 3 non-consecutive computer game pieces" do 
-      it "returns the index of the column that would result in a win + 1" do 
-        board = [['1', '2', '3', '4', '5', '6', '7'],
-                 ['-', '-', '-', '-', '-', '-', '-'],
-                 ['-', '-', '-', '-', '-', '-', '-'],
-                 ['-', '-', '-', '-', '-', '-', '-'],
-                 ['-', '-', '-', '-', '-', '-', '-'],
-                 ['-', '-', 'B', '-', 'B', 'B', '-'],
-                 ['-', '-', '-', '-', '-', '-', '-']]
-        current_player_piece = "B"
-        expect(player.almost_win_horizontal?(current_player_piece, board)).to eql(4)
+
+      context "there is a diagonal on the board with three computer game pieces in consecutive postitions, but the winning move 
+                is not available" do
+        it "returns false" do 
+          board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', '-', 'B', '-', '-'],
+                         ['-', '-', '-', 'B', '-', '-', '-'],
+                         ['-', '-', 'B', '-', '-', 'R', '-']]
+          current_player_piece = "B"
+          board.set_diagonals
+          expect(player.almost_win_diagonal?(current_player_piece, board.diagonals, board.board)).to be false
+        end
       end
 
+      context "there is a diagonal on the board with three computer game pieces in proper postition, and one missing
+              and the winning move is available" do
+        it "returns the index of the column that would result in a win + 1" do 
+          board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', '-', '-', 'B', '-'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', 'B', 'R', '-', '-'],
+                         ['-', '-', 'B', '-', 'R', '-', '-']]
+          current_player_piece = "B"
+          board.set_diagonals
+          expect(player.almost_win_diagonal?(current_player_piece, board.diagonals, board.board)).to eql(5)
+        end
+      end
+
+       context "there is a diagonal on the board with fewer than three computer game pieces in proper postition" do
+        it "returns the index of the column that would result in a win + 1" do 
+          board.board = [['1', '2', '3', '4', '5', '6', '7'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', '-', '-', '-', '-'],
+                         ['-', '-', '-', 'B', '-', '-', '-'],
+                         ['-', '-', 'B', '-', '-', '-', '-']]
+          current_player_piece = "B"
+          board.set_diagonals
+          expect(player.almost_win_diagonal?(current_player_piece, board.diagonals, board.board)).to be false
+        end
+      end
     end
-=end
+
+    describe "#almost_win_vertical?" do 
+      context "there is a column on the board with three computer game pieces in consecutive postitions" do 
+        it "returns the index of the column that would result in a win + 1" do 
+          board = [['1', '2', '3', '4', '5', '6', '7'],
+                   ['-', '-', '-', '-', '-', '-', '-'],
+                   ['-', '-', '-', '-', '-', '-', '-'],
+                   ['-', '-', '-', '-', '-', '-', '-'],
+                   ['-', '-', '-', '-', '-', 'B', '-'],
+                   ['-', '-', '-', '-', '-', 'B', '-'],
+                   ['-', '-', '-', '-', '-', 'B', '-']]
+          current_player_piece = "B"
+          expect(player.almost_win_vertical?(current_player_piece, board)).to eql(6)
+        end
+      end
+
+      context "there is a column on the board with fewer than three computer game pieces in consecutive postitions" do 
+        it "returns the index of the column that would result in a win + 1" do 
+          board = [['1', '2', '3', '4', '5', '6', '7'],
+                   ['-', '-', '-', '-', '-', '-', '-'],
+                   ['-', '-', '-', '-', '-', '-', '-'],
+                   ['-', '-', '-', '-', '-', '-', '-'],
+                   ['-', '-', '-', '-', '-', '-', '-'],
+                   ['-', '-', '-', '-', '-', 'B', '-'],
+                   ['-', '-', '-', '-', '-', 'B', '-']]
+          current_player_piece = "B"
+          expect(player.almost_win_vertical?(current_player_piece, board)).to be false
+        end
+      end
+    end
   end
 end 
