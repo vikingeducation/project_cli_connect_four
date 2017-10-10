5ea7dab56d575a97171ed91c1962a5b1278f6e75
mac718 <mac718@gmail.com>
2017-10-10 14:31:00 -0400

    fixed tests for Game.create_players

--- connect_four/spec/game_spec.rb
+++ connect_four/spec/game_spec.rb
@@ -13,10 +13,18 @@ describe Game do
 
   describe "#create_players" do 
     context "game type = 1" do 
+      before do 
+        allow(game).to receive(:gets).and_return('1')
+      end
+      it "intializes player_1 as a HumanPlayer and player_2 as a ComputerPlayer" do 
+        game.create_players
+        expect(game.player_1).to be_an_instance_of(HumanPlayer)
+      end
+
       it "intializes player_1 as a HumanPlayer and player_2 as a ComputerPlayer" do 
         game_type = 1
         game.create_players
-        expect(game.player_1).to be_an_instance_of(HumanPlayer)
+        expect(game.player_2).to be_an_instance_of(ComputerPlayer)
       end
     end
   end
